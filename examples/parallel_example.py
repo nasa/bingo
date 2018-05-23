d94e46b8b5cc84ad51bf7766ca90b550b65afd17
Geoffrey Bomarito <gbomarit@herman.larc.nasa.gov>
2018-05-23 14:03:42 -0400

    updated examplesto be easier to read

--- examples/parallel_example.py
+++ examples/parallel_example.py
@@ -3,7 +3,6 @@ example of regression done using the parallel island manager (islands done
 in parallel on multiple mpi processes)
 """
 
-import math
 import random
 import logging
 from mpi4py import MPI
@@ -26,15 +25,14 @@ def make_circle_data(data_size):
     """makes test data for circular constant regression"""
     x = np.empty([data_size, 2])
     for i, theta in enumerate(np.linspace(0, 3.14 * 2, data_size)):
-        x[i, 0] = math.cos(theta)
-        x[i, 1] = math.sin(theta)
-    y = None
-    return x, y
+        x[i, 0] = np.cos(theta)
+        x[i, 1] = np.sin(theta)
+    return x
 
 
 def make_norm_data(data_size):
     """makes test data for finding 3d norm with standard regression"""
-    n_lin = int(math.pow(data_size, 1.0/3)) + 1
+    n_lin = int(np.power(data_size, 1.0/3)) + 1
     x_1 = np.linspace(0, 5, n_lin)
     x_2 = np.linspace(0, 5, n_lin)
     x_3 = np.linspace(0, 5, n_lin)
@@ -42,6 +40,7 @@ def make_norm_data(data_size):
     x = x[np.random.choice(x.shape[0], data_size, replace=False), :]
     # make solution
     y = (np.linalg.norm(x, axis=1))
+    y = y.reshape([-1, 1])
 
     return x, y
 
@@ -62,6 +61,7 @@ def make_1d_data(data_size, test_num):
         x = np.linspace(-10, 10, data_size, False)
         y = x*x*np.exp(np.sin(x)) + x + np.sin(3.14159/4 - x*x*x)
     x = x.reshape([-1, 1])
+    y = y.reshape([-1, 1])
     return x, y
 
 
@@ -71,12 +71,11 @@ def make_sphere_data(data_size):
     for i in range(data_size):
         phi = (3.140*.9*i)/data_size+3.14*.05
         theta = (3.14*20*i)/data_size+3.14/2
-        x[i, 0] = math.cos(theta)*math.sin(phi)
-        x[i, 1] = math.sin(theta)*math.sin(phi)
-        x[i, 2] = math.cos(phi)
+        x[i, 0] = np.cos(theta)*np.sin(phi)
+        x[i, 1] = np.sin(theta)*np.sin(phi)
+        x[i, 2] = np.cos(phi)
         i += 1
-    y = None
-    return x, y
+    return x
 
 
 def make_sphere_data_changing_rad(data_size):
@@ -90,13 +89,12 @@ def make_sphere_data_changing_rad(data_size):
             radius += -0.2
         phi = (3.140 * .9 * i) / data_size + 3.14 * .05
         theta = (3.14 * 20 * i) / data_size + 3.14 / 2
-        x[i, 0] = math.cos(theta) * math.sin(phi) * radius
-        x[i, 1] = math.sin(theta) * math.sin(phi) * radius
-        x[i, 2] = math.cos(phi) * radius
+        x[i, 0] = np.cos(theta) * np.sin(phi) * radius
+        x[i, 1] = np.sin(theta) * np.sin(phi) * radius
+        x[i, 2] = np.cos(phi) * radius
         x[i, 3] = radius
         i += 1
-    y = None
-    return x, y
+    return x
 
 
 def main(max_steps, epsilon, data_size):
@@ -104,33 +102,42 @@ def main(max_steps, epsilon, data_size):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
-    # load data on rank 0
+    # load data on rank 0 and send it to all ranks
+    x_true = None
+    y_true = None
     if rank == 0:
-        # make data (uncomment whichever test data set you would like to use)
+        # --------------------------------------------
+        # MAKE DATA (uncomment one of the below lines)
+        # --------------------------------------------
 
         # standard regression
+        # (use with ExplicitTrainingData and StandardRegression)
         # x_true, y_true = make_1d_data(data_size, 1)
-        # x_true, y_true = make_1d_data(data_size, 2)
+        x_true, y_true = make_1d_data(data_size, 2)
         # x_true, y_true = make_1d_data(data_size, 3)
         # x_true, y_true = make_1d_data(data_size, 4)
-        x_true, y_true = make_norm_data(data_size)
+        # x_true, y_true = make_norm_data(data_size)
 
-        # constant regression
-        # x_true, y_true = make_circle_data(data_size)
+        # implicit regression
+        # (use with ImplicitTrainingData and ImplicitRegression)
+        # x_true = make_circle_data(data_size)
 
-    else:
-        x_true = None
-        y_true = None
     # then broadcast to all ranks
     x_true = MPI.COMM_WORLD.bcast(x_true, root=0)
     y_true = MPI.COMM_WORLD.bcast(y_true, root=0)
 
-    # make training data
+    # ------------------------------------------------
+    # MAKE TRAINING DATA  (uncomment one of the below)
+    # ------------------------------------------------
     training_data = ExplicitTrainingData(x_true, y_true)
+    # training_data = ImplicitTrainingData(x_true)
 
+    # ------------------------------------------------------------
+    # MAKE SOLUTION MANIPULATOR (uncomment one of the below blocks)
+    # ------------------------------------------------------------
 
-    # # make solution manipulator
-    # sol_manip = agm(x_true.shape[1], 64, nloads=2)
+    # using AGraph.py for the solution manipulator
+    # sol_manip = agm(x_true.shape[1], 32, nloads=2)
     # sol_manip.add_node_type(AGNodes.Add)
     # sol_manip.add_node_type(AGNodes.Subtract)
     # sol_manip.add_node_type(AGNodes.Multiply)
@@ -142,8 +149,7 @@ def main(max_steps, epsilon, data_size):
     # # sol_manip.add_node_type(AGNodes.Abs)
     # sol_manip.add_node_type(AGNodes.Sqrt)
 
-
-    # make solution manipulator
+    # using AGraphCpp.py for the solution manipulator
     sol_manip = AGraphCpp.AGraphCppManipulator(x_true.shape[1], 32, nloads=2)
     sol_manip.add_node_type(2)  # +
     sol_manip.add_node_type(3)  # -
@@ -151,18 +157,25 @@ def main(max_steps, epsilon, data_size):
     sol_manip.add_node_type(5)  # /
     sol_manip.add_node_type(12)  # sqrt
 
-    # make predictor manipulator
+    # ----------------------------------
+    # MAKE FITNESS PREDICTOR MANIPULATOR
+    # ----------------------------------
     pred_manip = fpm(128, data_size)
 
-    # make fitness metric
-    standard_regressor = StandardRegression(const_deriv=True)
+    # ------------------------------------------------
+    # MAKE FITNESS METRIC (uncomment one of the below)
+    # ------------------------------------------------
+    regressor = StandardRegression(const_deriv=True)
+    # regressor = ImplicitRegression()
 
-    # make and run island manager
+    # --------------------------------------
+    # MAKE SERIAL ISLAND MANAGER THEN RUN IT
+    # --------------------------------------
     islmngr = ParallelIslandManager(solution_training_data=training_data,
                                     solution_manipulator=sol_manip,
                                     predictor_manipulator=pred_manip,
                                     solution_pop_size=64,
-                                    fitness_metric=standard_regressor,
+                                    fitness_metric=regressor,
                                     solution_age_fitness=True)
     islmngr.run_islands(max_steps, epsilon, min_steps=1000,
                         step_increment=1000)
--- examples/serial_example.py
+++ examples/serial_example.py
@@ -2,6 +2,7 @@
 example of regression done using the serial island manager (islands done
 serially on a single process)
 """
+import random
 import numpy as np
 
 from bingo.AGraph import AGraphManipulator as agm
@@ -17,58 +18,114 @@ from bingocpp.build import bingocpp
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
-def main(max_steps, epsilon, data_size, data_range, n_islands):
+
+def make_circle_data(data_size):
+    """makes test data for circular constant regression"""
+    x = np.empty([data_size, 2])
+    for i, theta in enumerate(np.linspace(0, 3.14 * 2, data_size)):
+        x[i, 0] = np.cos(theta)
+        x[i, 1] = np.sin(theta)
+    return x
+
+
+def make_norm_data(data_size):
+    """makes test data for finding 3d norm with standard regression"""
+    n_lin = int(np.power(data_size, 1.0/3)) + 1
+    x_1 = np.linspace(0, 5, n_lin)
+    x_2 = np.linspace(0, 5, n_lin)
+    x_3 = np.linspace(0, 5, n_lin)
+    x = np.array(np.meshgrid(x_1, x_2, x_3)).T.reshape(-1, 3)
+    x = x[np.random.choice(x.shape[0], data_size, replace=False), :]
+    # make solution
+    y = (np.linalg.norm(x, axis=1))
+    y = y.reshape([-1, 1])
+
+    return x, y
+
+
+def make_1d_data(data_size, test_num):
+    """makes test data for 1d standard symbolic regression"""
+    x = np.empty(0)
+    if test_num == 1:
+        x = np.linspace(-2, 2, data_size, False)
+        y = x * x + 0.5
+    elif test_num == 2:
+        x = np.linspace(-5, 5, data_size, False)
+        y = 1.5*x*x - x*x*x
+    elif test_num == 3:
+        x = np.linspace(-3, 3, data_size, False)
+        y = np.exp(np.abs(x))*np.sin(x)
+    elif test_num == 4:
+        x = np.linspace(-10, 10, data_size, False)
+        y = x*x*np.exp(np.sin(x)) + x + np.sin(3.14159/4 - x*x*x)
+    x = x.reshape([-1, 1])
+    y = y.reshape([-1, 1])
+    return x, y
+
+
+def make_sphere_data(data_size):
+    """makes test data for spherical constant regression"""
+    x = np.empty([data_size, 3])
+    for i in range(data_size):
+        phi = (3.140*.9*i)/data_size+3.14*.05
+        theta = (3.14*20*i)/data_size+3.14/2
+        x[i, 0] = np.cos(theta)*np.sin(phi)
+        x[i, 1] = np.sin(theta)*np.sin(phi)
+        x[i, 2] = np.cos(phi)
+        i += 1
+    return x
+
+
+def make_sphere_data_changing_rad(data_size):
+    """makes test data for spherical constant regression with varying radius"""
+    x = np.empty([data_size, 4])
+    radius = 10
+    for i in range(data_size):
+        if random.random() > 0.5:
+            radius += 0.2
+        else:
+            radius += -0.2
+        phi = (3.140 * .9 * i) / data_size + 3.14 * .05
+        theta = (3.14 * 20 * i) / data_size + 3.14 / 2
+        x[i, 0] = np.cos(theta) * np.sin(phi) * radius
+        x[i, 1] = np.sin(theta) * np.sin(phi) * radius
+        x[i, 2] = np.cos(phi) * radius
+        x[i, 3] = radius
+        i += 1
+    return x
+
+
+def main(max_steps, epsilon, data_size, n_islands):
     """main regression function"""
 
-    # make data
-    # X = np.linspace(data_range[0], data_range[1], data_size, False)
+    # --------------------------------------------
+    # MAKE DATA (uncomment one of the below lines)
+    # --------------------------------------------
 
-    # Y = 1.5*X*X - X*X*X
-    # Y = np.exp(X) * np.sin(X)
+    # data for standard regression
+    # (use with ExplicitTrainingData and StandardRegression)
+    # x_true, y_true = make_1d_data(data_size, 1)
+    x_true, y_true = make_1d_data(data_size, 2)
+    # x_true, y_true = make_1d_data(data_size, 3)
+    # x_true, y_true = make_1d_data(data_size, 4)
+    # x_true, y_true = make_norm_data(data_size)
 
-    # X = X.reshape([-1, 1])
+    # data for implicit regression
+    # (use with ImplicitTrainingData and ImplicitRegression)
+    # x_true = make_circle_data(data_size)
 
+    # ------------------------------------------------
+    # MAKE TRAINING DATA  (uncomment one of the below)
+    # ------------------------------------------------
+    training_data = ExplicitTrainingData(x_true, y_true)
+    # training_data = ImplicitTrainingData(x_true)
 
-    # make data
-    # Xx = snake_walk()
+    # ------------------------------------------------------------
+    # MAKE SOLUTION MANIPULATOR (uncomment one of the below blocks)
+    # ------------------------------------------------------------
 
-    ########################### Circle problem
-    # create a radius, and fill an array(angle) with points depending on the data size
-    radius = 4
-    angle = np.linspace(0, 2 * np.pi, data_size)
-    # a variable to get the position in angle
-    position = 0
-    # create the array to hold the points representing the circle and fill the first position
-    circleArray = np.array([[radius * np.cos(angle[position])], [radius * np.sin(angle[position])]])
-    # loop through the data size to fill the rest of the circle Array
-    for ang in angle:
-        circleArray = np.append(circleArray, [[radius * np.cos(ang)], [radius * np.sin(ang)]], axis=1)
-
-    circleArray = np.transpose(circleArray)
-    # assign the array created with the points for circle to X
-    X = circleArray
-    ############################
-
-    # 1.5x0 squared - x1 cubed
-    # Yx = (1.5 * np.square(Xx[:, 0]) - np.power(Xx[:, 1], 3))
-    # Yx = (1.5 * (Xx[:, 0] * Xx[:, 0]) - (Xx[:, 1] * Xx[:, 1] * Xx[:, 1]))
-
-    # Yx = (Xx[:, 0] / Xx[:, 1])
-
-    # Xi = np.hstack((Xx, Yx))
-    # Yi = None
-
-    # X=Xx
-    # Y=Yx
-    # training_data = ExplicitTrainingData(X, Y)
-    # fitness_metric = StandardRegression()
-
-    training_data = bingocpp.ImplicitTrainingData(X)
-    fitness_metric = bingocpp.ImplicitRegression()
-
-    # make solution manipulator
-    # sol_manip = agm(X.shape[1], 16, nloads=2)
-    #
+    # using AGraph.py for the solution manipulator
+    # sol_manip = agm(x_true.shape[1], 32, nloads=2)
     # sol_manip.add_node_type(AGNodes.Add)
     # sol_manip.add_node_type(AGNodes.Subtract)
     # sol_manip.add_node_type(AGNodes.Multiply)
@@ -77,45 +134,45 @@ def main(max_steps, epsilon, data_size, data_range, n_islands):
     # # sol_manip.add_node_type(AGNodes.Log)
     # # sol_manip.add_node_type(AGNodes.Sin)
     # # sol_manip.add_node_type(AGNodes.Cos)
-    # sol_manip.add_node_type(AGNodes.Abs)
+    # # sol_manip.add_node_type(AGNodes.Abs)
+    # sol_manip.add_node_type(AGNodes.Sqrt)
 
-    # # debugging
-    # sol = sol_manip.generate()
-    # sol.command_list[0] = (AGNodes.Load_Data, (0,))
-    # sol.command_list[1] = (AGNodes.Load_Data, (1,))
-    # sol.command_list[2] = (AGNodes.Load_Const, (None,))
-    # sol.command_list[3] = (AGNodes.Exp, (0,))
-    # sol.command_list[4] = (AGNodes.Add, (2, 3))
-    # sol.command_list[5] = (AGNodes.Exp, (4,))
-    # sol.command_list[6] = (AGNodes.Subtract, (0, 1))
-    # sol.command_list[-1] = (AGNodes.Divide, (5, 6))
-
-    # make solution manipulator
-    sol_manip = bingocpp.AcyclicGraphManipulator(X.shape[1], 64, nloads=2)
+    # using AGraphCpp.py for the solution manipulator
+    sol_manip = AGraphCpp.AGraphCppManipulator(x_true.shape[1], 32, nloads=2)
     sol_manip.add_node_type(2)  # +
     sol_manip.add_node_type(3)  # -
     sol_manip.add_node_type(4)  # *
     sol_manip.add_node_type(5)  # /
+    sol_manip.add_node_type(12)  # sqrt
 
-    # make predictor manipulator
-    pred_manip = fpm(32, X.shape[0])
+    # ----------------------------------
+    # MAKE FITNESS PREDICTOR MANIPULATOR
+    # ----------------------------------
+    pred_manip = fpm(128, data_size)
 
-    # make and run island manager
+    # ------------------------------------------------
+    # MAKE FITNESS METRIC (uncomment one of the below)
+    # ------------------------------------------------
+    regressor = StandardRegression()
+    # regressor = ImplicitRegression()
+
+    # --------------------------------------
+    # MAKE SERIAL ISLAND MANAGER THEN RUN IT
+    # --------------------------------------
     islmngr = SerialIslandManager(n_islands,
                                   solution_training_data=training_data,
                                   solution_manipulator=sol_manip,
                                   predictor_manipulator=pred_manip,
-                                  fitness_metric=fitness_metric,
+                                  solution_pop_size=64,
+                                  fitness_metric=regressor,
+                                  solution_age_fitness=True
                                   )
-    # print("--------------------------")
-    # print("True fitness: %le" % islmngr.isles[0].solution_fitness_true(sol))
-    # print(sol.latexstring())
-
-    # islmngr.load_state('test.p')
     islmngr.run_islands(max_steps, epsilon, step_increment=100)
+
+    # example of saving/loading
     # islmngr.save_state('test.p')
     # islmngr.load_state('test.p')
-    #islmngr.run_islands(max_steps, epsilon, step_increment=100)
+    # islmngr.run_islands(max_steps, epsilon, step_increment=100)
 
 
 if __name__ == "__main__":
@@ -123,9 +180,8 @@ if __name__ == "__main__":
     MAX_STEPS = 1000
     CONVERGENCE_EPSILON = 1.0e-8
     DATA_SIZE = 100
-    DATA_RANGE = [-3, 3]
     N_ISLANDS = 2
 
     bingocpp.rand_init()
     
-    main(MAX_STEPS, CONVERGENCE_EPSILON, DATA_SIZE, DATA_RANGE, N_ISLANDS)
+    main(MAX_STEPS, CONVERGENCE_EPSILON, DATA_SIZE, N_ISLANDS)

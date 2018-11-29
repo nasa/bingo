fea5d3084a538041aaf6857ec79dfea8b6c34d56
Geoffrey Bomarito <gbomarit@herman.larc.nasa.gov>
2018-11-29 15:57:40 -0500

    added documentation to agraph backend

--- bingo/AGraph/Backend.py
+++ bingo/AGraph/Backend.py
@@ -1,9 +1,31 @@
+"""
+This module represents the python backend associated with the Agraph equation
+representation.  The backend is used to perform the most computationally
+demanding functions required by the Agraph.
+"""
+
 import numpy as np
 
 from bingo.AGraph import BackendOperators as Operators
 
 
 def simplify_and_evaluate(stack, x, constants):
+    """
+    Evauluate the equation associated with an Agraph, at the values x.
+    Simplification ensures that only the commands utilized in the result are
+    considered.
+
+    :param stack: the command stack associated with an equation.
+    :type stack: Nx3 numpy array of int. Where N is the number of commands in
+                 the stack.
+    :param x: values at which to evaluate the equations.
+    :type x: MxD numpy array of numeric. Where D is the number of dimensions
+             in x and M is the number of data points in x.
+    :param constants: numeric constants that are used in the equation
+    :type constants: list-like of numeric.
+    :return: value of the equation at x
+    :rtype: Mx1 numpy array of numeric
+    """
     used_commands_mask = get_utilized_commands(stack)
     forward_eval = _forward_eval_with_mask(stack, x, constants,
                                            used_commands_mask)
@@ -12,6 +34,26 @@ def simplify_and_evaluate(stack, x, constants):
 
 def simplify_and_evaluate_with_derivative(stack, x, constants,
                                           wrt_param_x_or_c):
+    """
+    Evauluate the derivatives of the equation associated with an Agraph, at the
+    values x.  Simplification ensures that only the commands utilized in the
+    result are considered.
+
+    :param stack: the command stack associated with an equation.
+    :type stack: Nx3 numpy array of int. Where N is the number of commands in
+                 the stack.
+    :param x: values at which to evaluate the equations.
+    :type x: MxD numpy array of numeric. Where D is the number of dimensions
+             in x and M is the number of data points in x.
+    :param constants: numeric constants that are used in the equation
+    :type constants: list-like of numeric of length=L
+    :param wrt_param_x_or_c: Take derivative with respect to x or constants.
+                             True signifies derivatives are wrt x. False
+                             signifies derivatives are wrt constants.
+    :type wrt_param_x_or_c: boolean
+    :return: Derivatives of all dimensions of x/constants at location x.
+    :rtype: MxD numpy array of numeric or MxL numpy array of numeric.
+    """
     used_commands_mask = get_utilized_commands(stack)
     return _evaluate_with_derivative_and_mask(stack, x, constants,
                                               used_commands_mask,
@@ -19,13 +61,23 @@ def simplify_and_evaluate_with_derivative(stack, x, constants,
 
 
 def get_utilized_commands(stack):
+    """
+    Find the commands (rows) of the stack that are utilized by the last command
+    of the stack.
+
+    :param stack: the command stack associated with an equation.
+    :type stack: Nx3 numpy array of int. Where N is the number of commands in
+                 the stack.
+    :return: Boolean values for whether each command is utilized.
+    :rtype: list of boolean of length N
+    """
     util = [False]*stack.shape[0]
     util[-1] = True
     for i in range(1, stack.shape[0]):
         operator, param1, param2 = stack[-i]
         if util[-i] and operator > 1:
             util[param1] = True
-            if Operators.is_arity_2_map[operator]:
+            if Operators.IS_ARITY_2_MAP[operator]:
                 util[param2] = True
     return util
 
@@ -35,7 +87,7 @@ def _forward_eval_with_mask(stack, x, constants, used_commands_mask):
     for i, command_is_used in enumerate(used_commands_mask):
         if command_is_used:
             operator, param1, param2 = stack[i]
-            forward_eval[i] = Operators.forward_eval_map[operator](param1,
+            forward_eval[i] = Operators.FORWARD_EVAL_MAP[operator](param1,
                                                                    param2,
                                                                    x,
                                                                    constants,
@@ -74,7 +126,6 @@ def _reverse_eval_with_mask(deriv_shape, deriv_wrt_operator, forward_eval,
             if operator == deriv_wrt_operator:
                 derivative[:, param1] += reverse_eval[i]
             else:
-                Operators.reverse_eval_map[operator](i, param1, param2,
+                Operators.REVERSE_EVAL_MAP[operator](i, param1, param2,
                                                      forward_eval, reverse_eval)
     return derivative
-
--- bingo/AGraph/BackendOperators.py
+++ bingo/AGraph/BackendOperators.py
@@ -1,51 +1,63 @@
+"""
+This module provides the python implementation of the functions for each
+mathematical operator used in Agraph
+
+Attributes:
+This module provides 3 dictionaries that can be used for mapping operator
+numbers to the defined functions
+- IS_ARITY_2_MAP: states whether each operator has arity 2 (as opposed to 1)
+- FORWARD_EVAL_MAP: map of operator number to evaluation function
+- REVERSE_EVAL_MAP: map of operator number to derivative evaluation function
+"""
+
 import numpy as np
 np.seterr(divide='ignore', invalid='ignore')
 
 
 # Load x column
-def _loadx_forward_eval(param1, param2, x, constants, forward_eval):
+def _loadx_forward_eval(param1, _param2, x, _constants, _forwardeval):
     return x[:, param1]
 
 
-def _loadx_reverse_eval(reverse_index, param1, param2, forward_eval,
-                        reverse_eval):
+def _loadx_reverse_eval(_reverseindex, _param1, _param2, _forwardeval,
+                        _reverseeval):
     pass
 
 
 # Load constant
-def _loadc_forward_eval(param1, param2, x, constants, forward_eval):
+def _loadc_forward_eval(param1, _param2, _x, constants, _forwardeval):
     return constants[param1]
 
 
-def _loadc_reverse_eval(reverse_index, param1, param2, forward_eval,
-                        reverse_eval):
+def _loadc_reverse_eval(_reverseindex, _param1, _param2, _forwardeval,
+                        _reverseeval):
     pass
 
 
 # Addition
-def _add_forward_eval(param1, param2, x, constants, forward_eval):
+def _add_forward_eval(param1, param2, _x, _constants, forward_eval):
     return forward_eval[param1] + forward_eval[param2]
 
 
-def _add_reverse_eval(reverse_index, param1, param2, forward_eval,
+def _add_reverse_eval(reverse_index, param1, param2, _forwardeval,
                       reverse_eval):
     reverse_eval[param1] += reverse_eval[reverse_index]
     reverse_eval[param2] += reverse_eval[reverse_index]
 
 
 # Subtraction
-def _subtract_forward_eval(param1, param2, x, constants, forward_eval):
+def _subtract_forward_eval(param1, param2, _x, _constants, forward_eval):
     return forward_eval[param1] - forward_eval[param2]
 
 
-def _subtract_reverse_eval(reverse_index, param1, param2, forward_eval,
+def _subtract_reverse_eval(reverse_index, param1, param2, _forwardeval,
                            reverse_eval):
     reverse_eval[param1] += reverse_eval[reverse_index]
     reverse_eval[param2] -= reverse_eval[reverse_index]
 
 
 # Multiplication
-def _multiply_forward_eval(param1, param2, x, constants, forward_eval):
+def _multiply_forward_eval(param1, param2, _x, _constants, forward_eval):
     return forward_eval[param1] * forward_eval[param2]
 
 
@@ -56,7 +68,7 @@ def _multiply_reverse_eval(reverse_index, param1, param2, forward_eval,
 
 
 # Division
-def _divide_forward_eval(param1, param2, x, constants, forward_eval):
+def _divide_forward_eval(param1, param2, _x, _constants, forward_eval):
     return forward_eval[param1] / forward_eval[param2]
 
 
@@ -68,51 +80,51 @@ def _divide_reverse_eval(reverse_index, param1, param2, forward_eval,
 
 
 # Sine
-def _sin_forward_eval(param1, param2, x, constants, forward_eval):
+def _sin_forward_eval(param1, _param2, _x, _constants, forward_eval):
     return np.sin(forward_eval[param1])
 
 
-def _sin_reverse_eval(reverse_index, param1, param2, forward_eval,
+def _sin_reverse_eval(reverse_index, param1, _param2, forward_eval,
                       reverse_eval):
     reverse_eval[param1] += \
         reverse_eval[reverse_index] * np.cos(forward_eval[param1])
 
 
 # Cosine
-def _cos_forward_eval(param1, param2, x, constants, forward_eval):
+def _cos_forward_eval(param1, _param2, _x, _constants, forward_eval):
     return np.cos(forward_eval[param1])
 
 
-def _cos_reverse_eval(reverse_index, param1, param2, forward_eval,
+def _cos_reverse_eval(reverse_index, param1, _param2, forward_eval,
                       reverse_eval):
     reverse_eval[param1] -= \
         reverse_eval[reverse_index] * np.sin(forward_eval[param1])
 
 
 # Exponential
-def _exp_forward_eval(param1, param2, x, constants, forward_eval):
+def _exp_forward_eval(param1, _param2, _x, _constants, forward_eval):
     return np.exp(forward_eval[param1])
 
 
-def _exp_reverse_eval(reverse_index, param1, param2, forward_eval,
+def _exp_reverse_eval(reverse_index, param1, _param2, forward_eval,
                       reverse_eval):
     reverse_eval[param1] += reverse_eval[reverse_index] *\
                             forward_eval[reverse_index]
 
 
 # Natural logarithm
-def _log_forward_eval(param1, param2, x, constants, forward_eval):
+def _log_forward_eval(param1, _param2, _x, _constants, forward_eval):
     return np.log(np.abs(forward_eval[param1]))
 
 
-def _log_reverse_eval(reverse_index, param1, param2, forward_eval,
+def _log_reverse_eval(reverse_index, param1, _param2, forward_eval,
                       reverse_eval):
     reverse_eval[param1] += reverse_eval[reverse_index] /\
                             forward_eval[param1]
 
 
 # Power
-def _pow_forward_eval(param1, param2, x, constants, forward_eval):
+def _pow_forward_eval(param1, param2, _x, _constants, forward_eval):
     return np.power(np.abs(forward_eval[param1]), forward_eval[param2])
 
 
@@ -127,22 +139,22 @@ def _pow_reverse_eval(reverse_index, param1, param2, forward_eval,
 
 
 # Absolute value
-def _abs_forward_eval(param1, param2, x, constants, forward_eval):
+def _abs_forward_eval(param1, _param2, _x, _constants, forward_eval):
     return np.abs(forward_eval[param1])
 
 
-def _abs_reverse_eval(reverse_index, param1, param2, forward_eval,
+def _abs_reverse_eval(reverse_index, param1, _param2, forward_eval,
                       reverse_eval):
     reverse_eval[param1] += reverse_eval[reverse_index] *\
                             np.sign(forward_eval[param1])
 
 
 # Square root
-def _sqrt_forward_eval(param1, param2, x, constants, forward_eval):
+def _sqrt_forward_eval(param1, _param2, _x, _constants, forward_eval):
     return np.sqrt(np.abs(forward_eval[param1]))
 
 
-def _sqrt_reverse_eval(reverse_index, param1, param2, forward_eval,
+def _sqrt_reverse_eval(reverse_index, param1, _param2, forward_eval,
                        reverse_eval):
     reverse_eval[param1] += 0.5*reverse_eval[reverse_index] /\
                             forward_eval[reverse_index] *\
@@ -150,7 +162,7 @@ def _sqrt_reverse_eval(reverse_index, param1, param2, forward_eval,
 
 
 # Operator maps
-is_arity_2_map = {0: False,
+IS_ARITY_2_MAP = {0: False,
                   1: False,
                   2: True,
                   3: True,
@@ -164,7 +176,7 @@ is_arity_2_map = {0: False,
                   11: False,
                   12: False}
 
-forward_eval_map = {0: _loadx_forward_eval,
+FORWARD_EVAL_MAP = {0: _loadx_forward_eval,
                     1: _loadc_forward_eval,
                     2: _add_forward_eval,
                     3: _subtract_forward_eval,
@@ -178,7 +190,7 @@ forward_eval_map = {0: _loadx_forward_eval,
                     11: _abs_forward_eval,
                     12: _sqrt_forward_eval}
 
-reverse_eval_map = {0: _loadx_reverse_eval,
+REVERSE_EVAL_MAP = {0: _loadx_reverse_eval,
                     1: _loadc_reverse_eval,
                     2: _add_reverse_eval,
                     3: _subtract_reverse_eval,

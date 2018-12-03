e086a05d182531d2798ea15df12eaac413a32fdc
Geoffrey Bomarito <gbomarit@herman.larc.nasa.gov>
2018-12-03 10:30:40 -0500

    added equation individual to tests

--- test_genetic_individual.py
+++ test_genetic_individual.py
@@ -4,10 +4,10 @@
 import pytest
 import numpy as np
 
-from bingo.GeneticIndividual import GeneticIndividual
+from bingo.GeneticIndividual import GeneticIndividual, EquationIndividual
 
 
-class InvalidChild(GeneticIndividual):
+class InvalidGeneticIndividualChild(GeneticIndividual):
     def __str__(self):
         super().__str__()
 
@@ -21,9 +21,35 @@ class InvalidChild(GeneticIndividual):
         super().set_local_optimization_params(params)
 
 
+class InvalidEquationIndividualChild(EquationIndividual,
+                                     InvalidGeneticIndividualChild):
+    def evaluate_equation_at(self, x):
+        super().evaluate_equation_at(x)
+
+    def evaluate_equation_with_x_gradient_at(self, x):
+        super().evaluate_equation_with_x_gradient_at(x)
+
+    def evaluate_equation_with_local_opt_gradient_at(self, x):
+        super().evaluate_equation_with_local_opt_gradient_at(x)
+
+    def get_latex_string(self):
+        super().get_latex_string()
+
+    def get_console_string(self):
+        super().get_console_string()
+
+    def get_complexity(self):
+        super().get_complexity()
+
+
 @pytest.fixture
 def bad_gi():
-    return InvalidChild()
+    return InvalidGeneticIndividualChild()
+
+
+@pytest.fixture
+def bad_ei():
+    return InvalidEquationIndividualChild()
 
 
 def test_raises_error_construct_genetic_individual():
@@ -31,7 +57,7 @@ def test_raises_error_construct_genetic_individual():
         _ = GeneticIndividual()
 
 
-def test_raises_error_using_super_on_derived_classes(bad_gi):
+def test_raises_error_using_super_on_gi_derived_classes(bad_gi):
     with pytest.raises(NotImplementedError):
         str(bad_gi)
     with pytest.raises(NotImplementedError):
@@ -40,3 +66,19 @@ def test_raises_error_using_super_on_derived_classes(bad_gi):
         bad_gi.get_number_local_optimization_params()
     with pytest.raises(NotImplementedError):
         bad_gi.set_local_optimization_params([1.0])
+
+
+def test_raises_error_using_super_on_ei_derived_classes(bad_ei):
+    x = np.ones((1, 1))
+    with pytest.raises(NotImplementedError):
+        bad_ei.evaluate_equation_at(x)
+    with pytest.raises(NotImplementedError):
+        bad_ei.evaluate_equation_with_x_gradient_at(x)
+    with pytest.raises(NotImplementedError):
+        bad_ei.evaluate_equation_with_local_opt_gradient_at(x)
+    with pytest.raises(NotImplementedError):
+        bad_ei.get_latex_string()
+    with pytest.raises(NotImplementedError):
+        bad_ei.get_console_string()
+    with pytest.raises(NotImplementedError):
+        bad_ei.get_complexity()

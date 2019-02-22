2fa155965b6c01ede4616d2640969329847ef329
gbomarito <geoffrey.f.bomarito@nasa.gov>
2019-02-22 14:36:34 -0500

    Update examples/OneMaxExample.py
    Co-Authored-By: veradiana <31020322+veradiana@users.noreply.github.com>

--- examples/OneMaxExample.py
+++ examples/OneMaxExample.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from Base.FitnessEvaluator import FitnessEvaluator
 from Base.Crossover import Crossover
-from Base.Variation import Variation
+from bingo.Base.Variation import Variation
 from bingo.Base.Evaluation import Evaluation
 from bingo.Base.Selection import Selection
 from bingo.EA.SimpleEa import SimpleEa

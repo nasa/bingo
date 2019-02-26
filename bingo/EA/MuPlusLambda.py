e437a8f841b386a10a5880d714772019439ed7df
Diana Vera <diana.vera@nasa.gov>
2019-02-26 10:27:50 -0500

    added MuPlusLambda ea, plus docstrings

---
+++ bingo/EA/MuPlusLambda.py
@@ -0,0 +1,35 @@
+"""The "Mu + Lambda"
+
+This module defines the basis of the "mu plus lambda"
+evolutionary algorithm in bingo analyses.
+"""
+from bingo.Base.EvolutionaryAlgorithm import EvolutionaryAlgorithm
+
+class MuPlusLambda(EvolutionaryAlgorithm):
+    """The algorithm used to perform generational steps.
+
+    A class for the "mu plus lambda" evolutionary algorithm in bingo.
+    """
+    def __init__(self, variation, evaluation, selection):
+        self._variation = variation
+        self._evaluation = evaluation
+        self._selection = selection
+
+    def generational_step(self, population):
+        """Performs selection on individuals.
+
+        Parameters
+        ----------
+        population : list of Chromosome
+                     The population at the start of the generational step
+
+        Returns
+        -------
+        list of Chromosome :
+            The next generation of the population
+        """
+        self._evaluation(population)
+        number_offspring = 20
+        offspring = self._variation(population, number_offspring)
+        self._evaluation(offspring)
+        return self._selection(population + offspring, len(population))

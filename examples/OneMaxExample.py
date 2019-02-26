f6e205fe2ba156eb6cbf343ab04c3601c6e4a4e6
Diana Vera <diana.vera@nasa.gov>
2019-02-26 15:47:33 -0500

    refactored to use basic Island to execute generational steps

--- examples/OneMaxExample.py
+++ examples/OneMaxExample.py
@@ -7,8 +7,11 @@ from bingo.Base.Evaluation import Evaluation
 from bingo.Base.Selection import Selection
 from bingo.EA.SimpleEa import SimpleEa
 from bingo.EA.VarAnd import VarAnd
+from bingo.EA.VarOr import VarOr
+from bingo.EA.MuPlusLambda import MuPlusLambda
 from bingo.EA.TournamentSelection import Tournament
 from bingo.EA.SimpleEvaluation import SimpleEvaluation
+from bingo.EA.BasicIsland import Island
 from bingo.MultipleValues import *
 
 class MultipleValueFitnessEvaluator(FitnessEvaluator):
@@ -20,33 +23,24 @@ class MultipleValueFitnessEvaluator(FitnessEvaluator):
 def mutation_onemax_specific():
     return np.random.choice([True, False])
 
-def generate_population():
-    generator = MultipleValueGenerator(mutation_onemax_specific, 10)
-    population = [generator() for i in range(25)]
-    return population
-
 def execute_generational_steps():
-    population = generate_population()
-    selection = Tournament(10)
     crossover = SinglePointCrossover()
     mutation = SinglePointMutation(mutation_onemax_specific)
+    var_or = VarOr(crossover, mutation, 0.4, 0.4)
+    selection = Tournament(10)
     fitness = MultipleValueFitnessEvaluator()
-    evaluation = SimpleEvaluation(fitness)
-    variation = VarAnd(crossover, mutation, 0.8, 0.8)
-    ea = SimpleEa(variation, evaluation, selection)
-
-    for i in range(10):
-        next_gen = ea.generational_step(population)
+    evaluator = SimpleEvaluation(fitness)
+    ea = MuPlusLambda(var_or, evaluator, selection)
+    generator = MultipleValueGenerator(mutation_onemax_specific, 10)
+    island = Island(ea, generator, 25)
+    for i in range(50):
+        next_gen = island.execute_generational_step()
         print("\nGeneration #", i)
         print("----------------------\n")
         report_max_min_mean_fitness(next_gen)
-        print("\nparents: \n")
-        for indv in population:
-            print(indv.list_of_values)
-        print("\noffspring: \n")
+        print("\npopulation: \n")
         for indv in next_gen:
             print(indv.list_of_values)
-        population = next_gen
 
 def report_max_min_mean_fitness(population):
     fitness = [indv.fitness for indv in population]

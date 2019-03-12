dce5da14b994f5592bec5126fafe9c2d942607f7
Tyler Townsend <ttown@knights.ucf.edu>
2019-03-12 13:58:35 -0400

    updated random indv selection, refactored AgeFitness, test for pareto front, check that algorithm stops when target pop size acheived

--- bingo/AgeFitness.py
+++ bingo/AgeFitness.py
@@ -9,8 +9,7 @@ class AgeFitness(Selection):
     WORST_CASE_FACTOR = 50
 
     @argument_validation(selection_size={">=": 2})
-    def __init__(self, selection_size=2, remove_equals=True):
-        self._remove_equals = remove_equals
+    def __init__(self, selection_size=2):
         self._selection_size = selection_size
         self._selected_indices = []
         self._population_index_array = np.array([])
@@ -31,61 +30,36 @@ class AgeFitness(Selection):
               self._selection_attempts < start_pop_size * self.WORST_CASE_FACTOR:
 
             self._get_unique_random_individuals(population, num_removed)
-            indexes_of_indvs_removed = self._get_individuals_for_removal(population)
-            self._remove_indviduals(indexes_of_indvs_removed, num_removed)
+            removed_indv_indexs = self._get_individuals_for_removal(
+                population, target_population_size, num_removed)
+            self._remove_indviduals(removed_indv_indexs, num_removed)
 
             self._selection_attempts += 1
-            self._selected_indices.clear()
 
         return self._update_population(population, num_removed)
 
-    def _remove_indviduals(self, to_remove_list, num_removed):
-        while to_remove_list:
-            selection_index = to_remove_list.pop()
-            if num_removed[0] in to_remove_list:
-                to_remove_list.remove(num_removed[0])
-                to_remove_list.add(selection_index)
-            self._swap(self._population_index_array, num_removed[0], selection_index)
-            num_removed[0] += 1
-
-    def _swap(self, array, index_1, index_2):
-        array[index_1], array[index_2] = array[index_2], array[index_1]  
-
-    def _update_population(self, population, num_removed):
-        new_population = []
-        for kept_index in range(num_removed[0], len(population)):
-            indv = self._get_indvidual(population, kept_index)
-            new_population.append(indv)
-        return new_population
-
     def _get_unique_random_individuals(self, population, num_removed):
-        used_indvs = set()
-        self._selected_indices.append(self._rand_int(num_removed[0], len(population)))
-        used_indvs.add(self._selected_indices[0])
+        index_range = range(num_removed[0], len(population))
+        selection_size = self._selection_size \
+            if self._selection_size <= len(index_range) \
+            else len(index_range)
+        self._selected_indices = np.random.choice(index_range,
+                                                  selection_size,
+                                                  replace=False)
 
-        start = num_removed[0] + 1
-        stop = num_removed[0] + self._selection_size \
-            if self._selection_size < len(population) - num_removed[0] \
-            else len(population)
-
-        for list_index in range(start, stop):
-            val = self._rand_int(num_removed[0], len(population))
-            while val in used_indvs:
-                val = self._rand_int(num_removed[0], len(population))
-            self._selected_indices.append(val)
-            used_indvs.add(self._selected_indices[list_index-num_removed[0]])
-    
-    def _rand_int(self, start, stop):
-        return np.random.randint(start, stop)
-
-    def _get_individuals_for_removal(self, population):
+    # TODO look into optimizing. Possibly greedy approach
+    def _get_individuals_for_removal(self, population,
+                                     target_population_size, num_removed):
         to_be_removed = set()
+        num_remaining = len(population) - num_removed[0]
         for i in range(len(self._selected_indices)-1):
             for j in range(i+1, len(self._selected_indices)):
                 indv_index_1 = self._selected_indices[i]
                 indv_index_2 = self._selected_indices[j]
                 self._update_removal_set(population, indv_index_1,
                                          indv_index_2, to_be_removed)
+                if num_remaining - len(to_be_removed) == target_population_size:
+                    return to_be_removed
         return to_be_removed
 
     def _update_removal_set(self, population, indv_index_1,
@@ -101,7 +75,7 @@ class AgeFitness(Selection):
             removal_set.add(indv_index_2)
         elif self._one_dominates_all(indv_2, indv_1):
             removal_set.add(indv_index_1)
-        elif (indv_1.genetic_age == indv_2.genetic_age) and self._remove_equals:
+        elif (indv_1.genetic_age == indv_2.genetic_age):
             if indv_1.fitness <= indv_2.fitness:
                 removal_set.add(indv_index_2)
             else:
@@ -117,3 +91,23 @@ class AgeFitness(Selection):
 
     def _indv_has_nans(self, indiviudal):
         return np.any(np.isnan(indiviudal.fitness))
+
+    def _remove_indviduals(self, to_remove_list, num_removed):
+        while to_remove_list:
+            selection_index = to_remove_list.pop()
+            if num_removed[0] in to_remove_list:
+                to_remove_list.remove(num_removed[0])
+                to_remove_list.add(selection_index)
+            self._swap(self._population_index_array,
+                       num_removed[0], selection_index)
+            num_removed[0] += 1
+
+    def _swap(self, array, index_1, index_2):
+        array[index_1], array[index_2] = array[index_2], array[index_1]
+
+    def _update_population(self, population, num_removed):
+        new_population = []
+        for kept_index in range(num_removed[0], len(population)):
+            indv = self._get_indvidual(population, kept_index)
+            new_population.append(indv)
+        return new_population
--- tests/EA/test_age_fitness.py
+++ tests/EA/test_age_fitness.py
@@ -2,13 +2,9 @@ import pytest
 import numpy as np
 
 from bingo.Base.FitnessEvaluator import FitnessEvaluator
-from bingo.Base.Mutation import Mutation
-from bingo.Base.Crossover import Crossover
-from bingo.Base.Variation import Variation
-from bingo.Base.Evaluation import Evaluation
-from bingo.Base.Selection import Selection
 from bingo.EA.SimpleEvaluation import SimpleEvaluation
-from bingo.MultipleValues import MultipleValueGenerator, MultipleValueChromosome
+from bingo.MultipleValues import MultipleValueGenerator, \
+                                 MultipleValueChromosome
 from bingo.AgeFitness import AgeFitness
 
 INITIAL_POP_SIZE = 10
@@ -47,6 +43,11 @@ def strong_population():
     return [generator() for i in range(INITIAL_POP_SIZE)]
 
 @pytest.fixture
+def weak_population():
+    generator = MultipleValueGenerator(return_false, COMPLEX_INDV_SIZE)
+    return [generator() for i in range(INITIAL_POP_SIZE)]
+
+@pytest.fixture
 def non_dominated_population():
     young_weak = MultipleValueChromosome([False, False])
     middle_average = MultipleValueChromosome([False, True])
@@ -61,70 +62,121 @@ def all_dominated_population(non_dominated_population):
     non_dominated_population[2].genetic_age = 0
     return non_dominated_population
 
+@pytest.fixture
+def pareto_front_population():
+    size_of_list = 6
+    population = []
+    for i in range (size_of_list):
+        values = [False]*(size_of_list - i) + [True]*(i)
+        indv = MultipleValueChromosome(values)
+        indv.genetic_age = i
+        population.append(indv)
+    return population
 
-def test_age_fitness_selection_remove_one_weak(strong_population, weak_indvidual):
-    age_fitness_selection = AgeFitness(remove_equals=False)
-    indv_population = strong_population + [weak_indvidual]
+@pytest.fixture
+def evaluator():
     fitness = MultipleValueFitnessEvaluator()
     evaluator = SimpleEvaluation(fitness)
-    evaluator(indv_population)
-
-    new_population = age_fitness_selection(indv_population, TARGET_POP_SIZE)
-
-    weak_indv_removed = True
-    for i, indv in enumerate(new_population):
-        if False in indv.list_of_values:
-            weak_indv_removed = False
-            break
-    assert weak_indv_removed
-    assert len(new_population) == len(strong_population)
+    return evaluator
 
 def test_target_population_size_is_valid(strong_population):
     age_fitness_selection = AgeFitness()
     with pytest.raises(ValueError):
         age_fitness_selection(strong_population, len(strong_population) + 1)
 
-def test_none_selected_for_removal(non_dominated_population):
+def test_none_selected_for_removal(non_dominated_population, evaluator):
     age_fitness_selection = AgeFitness()
-    fitness = MultipleValueFitnessEvaluator()
-    evaluator = SimpleEvaluation(fitness)
     evaluator(non_dominated_population)
 
-    new_population = age_fitness_selection(non_dominated_population, 1)
+    target_pop_size = 1
+    new_population = age_fitness_selection(non_dominated_population,
+                                           target_pop_size)
     assert len(new_population) == len(non_dominated_population)
 
-def test_all_but_one_removed(all_dominated_population):
+def test_all_but_one_removed(all_dominated_population, evaluator):
     age_fitness_selection = AgeFitness()
-    fitness = MultipleValueFitnessEvaluator()
-    evaluator = SimpleEvaluation(fitness)
     evaluator(all_dominated_population)
 
-    new_population = age_fitness_selection(all_dominated_population, 1)
-    assert len(new_population) == 1
+    target_pop_size = 1
+    new_population = age_fitness_selection(all_dominated_population,
+                                           target_pop_size)
+    assert len(new_population) == target_pop_size 
 
-def test_all_but_one_removed_large_selection_size(strong_population, weak_indvidual):
+def test_all_but_one_removed_large_selection_size(strong_population,
+                                                  weak_indvidual,
+                                                  evaluator):
     population = strong_population +[weak_indvidual]
-    age_fitness_selection = AgeFitness(selection_size=10)
-    fitness = MultipleValueFitnessEvaluator()
-    evaluator = SimpleEvaluation(fitness)
     evaluator(population)
 
-    new_population = age_fitness_selection(population, 1)
-    assert len(new_population) == 1
+    age_fitness_selection = AgeFitness(selection_size=10)
+
+    target_pop_size=1
+    new_population = age_fitness_selection(population, target_pop_size)
+
+    assert len(new_population) == target_pop_size
     assert new_population[0].list_of_values == [True]
     assert age_fitness_selection._selection_attempts == 2
 
-def test_one_iteration(weak_indvidual, fit_individual):
+def test_all_removed_in_one_iteration(weak_indvidual,
+                                      fit_individual,
+                                      evaluator):
     population = [weak_indvidual for i in range(10)] + [fit_individual]
-    age_fitness_selection = AgeFitness(selection_size=len(population))
-    fitness = MultipleValueFitnessEvaluator()
-    evaluator = SimpleEvaluation(fitness)
     evaluator(population)
 
-    new_population = age_fitness_selection(population, 1)
-    assert len(new_population) == 1
+    age_fitness_selection = AgeFitness(selection_size=len(population))
+
+    target_pop_size = 1
+    new_population = age_fitness_selection(population, target_pop_size)
+
+    assert len(new_population) == target_pop_size
     assert new_population[0].list_of_values == [True]
     assert age_fitness_selection._selection_attempts == 1
 
-if __name__ == '__main__':
-    test_all_but_one_removed_large_selection_size(strong_population(), weak_indvidual())
+def test_selection_size_larger_than_population(weak_population, fit_individual, evaluator):
+    population = weak_population + [fit_individual]
+    evaluator(population)
+
+    age_fitness_selection = AgeFitness(selection_size=(len(population)+100))
+
+    target_pop_size = 2
+    new_population = age_fitness_selection(population, target_pop_size)
+
+    assert len(new_population) == target_pop_size
+    assert age_fitness_selection._selection_attempts == 1
+    count = 1
+    for indv in new_population:
+        if not any(indv.list_of_values):
+            count *= 2
+        elif all(indv.list_of_values):
+            count *= 3
+
+    assert count == 6
+
+def test_keep_pareto_front_miss_target_pop_size(pareto_front_population, evaluator):
+    list_size = len(pareto_front_population[0].list_of_values)
+    list_one = [False]*int(list_size/2)+[True]*int((list_size+1)/2)
+    list_two = [False]*int((list_size+1)/2)+[True]*int(list_size/2)
+
+    selected_indv_one = MultipleValueChromosome(list_one)
+    selected_indv_two = MultipleValueChromosome(list_two)
+
+    selected_indv_one.genetic_age = list_size
+    selected_indv_two.genetic_age = list_size + 1
+
+    population = pareto_front_population + [selected_indv_one, selected_indv_two]
+    evaluator(population)
+
+    age_fitness_selection = AgeFitness(selection_size=len(population))
+    new_population = age_fitness_selection(population, TARGET_POP_SIZE)
+
+    assert len(new_population) == len(pareto_front_population)
+
+    selected_indvs_removed = True
+    for indv in new_population:
+        if (indv.genetic_age == selected_indv_one and \
+                indv.list_of_values == list_one) or \
+                (indv.genetic_age == selected_indv_two and \
+                indv.list_of_values == list_two):
+            selected_indvs_removed = False
+            break
+    assert selected_indvs_removed

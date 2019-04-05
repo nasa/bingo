75ceb5a4ae68a2940c9ea281df7d528f15c5079d
Tyler Townsend <ttown@knights.ucf.edu>
2019-04-05 09:41:07 -0400

    updated AGraph example: fixed error in data init. Formmatting issue fixed in tests agraph

--- examples/AgraphExample.py
+++ examples/AgraphExample.py
@@ -6,8 +6,10 @@ import numpy as np
 from bingo.SymbolicRegression.AGraph.AGraphCrossover import AGraphCrossover
 from bingo.SymbolicRegression.AGraph.AgraphMutation import AGraphMutation
 from bingo.SymbolicRegression.AGraph.AGraphGenerator import AGraphGenerator
-from bingo.SymbolicRegression.AGraph.ComponentGenerator import ComponentGenerator
-from bingo.SymbolicRegression.ExplicitRegression import ExplicitRegression, ExplicitTrainingData
+from bingo.SymbolicRegression.AGraph.ComponentGenerator \
+    import ComponentGenerator
+from bingo.SymbolicRegression.ExplicitRegression import ExplicitRegression, \
+                                                        ExplicitTrainingData
 
 from bingo.Base.AgeFitnessEA import AgeFitnessEA
 from bingo.Base.Evaluation import Evaluation
@@ -16,6 +18,12 @@ from bingo.Base.ContinuousLocalOptimization import ContinuousLocalOptimization
 
 POP_SIZE = 100
 STACK_SIZE = 10
+MUTATION_PROBABILITY = 0.4
+CROSSOVER_PROBABILITY = 0.4
+NUM_POINTS = 100
+START = -10
+STOP = 10
+ERROR_TOLERANCE = 10e-6
 
 def init_x_vals(start, stop, num_points):
     return np.linspace(start, stop, num_points).reshape([-1, 1])
@@ -23,8 +31,8 @@ def init_x_vals(start, stop, num_points):
 def equation_eval(x):
     return x**2 + 3.5*x**3
 
-def execute_generational_steps():
-    x = init_x_vals(-10, 10, 100)
+def main():
+    x = init_x_vals(START, STOP, NUM_POINTS)
     y = equation_eval(x)
     training_data = ExplicitTrainingData(x, y)
 
@@ -40,22 +48,21 @@ def execute_generational_steps():
 
     fitness = ExplicitRegression(training_data=training_data)
     local_opt_fitness = ContinuousLocalOptimization(fitness, algorithm='lm')
-    evaluator = SimpleEvaluation(local_opt_fitness)
+    evaluator = Evaluation(local_opt_fitness)
 
     ea = AgeFitnessEA(evaluator, agraph_generator, crossover,
-                      mutation, 0.4, 0.4, POP_SIZE)
+                      mutation, MUTATION_PROBABILITY,
+                      CROSSOVER_PROBABILITY, POP_SIZE)
 
     island = Island(ea, agraph_generator, POP_SIZE)
 
-    curr_fitness = 10e6
     i = 0
-    while (curr_fitness > 10e-6):
+    while island.best_individual().fitness > 10e-6:
         island.execute_generational_step()
         print("\nGeneration #", i)
         i += 1
         print("-"*80, "\n")
         report_max_min_mean_fitness(island.population)
-        curr_fitness = island.best_individual().fitness
 
     print("Success!", island.best_individual().get_latex_string())
 
@@ -65,9 +72,5 @@ def report_max_min_mean_fitness(population):
     print("Min fitness: \t", np.min(fitness))
     print("Mean fitness: \t", np.mean(fitness))
 
-def main():
-    execute_generational_steps()
-
 if __name__ == '__main__':
     main()
-

249a4f96faa990b53098299bda92589d60b716c9
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2019-05-31 11:02:36 -0400

    added pareto front

---
+++ bingo/Base/ParetoFront.py
@@ -0,0 +1,47 @@
+from bingo.Base.HallOfFame import HallOfFame
+
+
+class ParetoFront(HallOfFame):
+
+    def __init__(self, secondary_key, primary_key=None,
+                 similarity_function=None):
+        super().__init__(max_size=None,
+                         key_function=primary_key,
+                         similarity_function=similarity_function)
+        self._key_func_2 = secondary_key
+
+    def update(self, population):
+        for indv in population:
+            if self._not_dominated(indv) and self._not_similar(indv):
+                self._remove_dominated_pf_memebers(indv)
+                self.insert(indv)
+
+    def _not_dominated(self, individual):
+        for hof_memeber in self:
+            if self._first_dominates(hof_memeber, individual):
+                return False
+        return True
+
+    def _first_dominates(self, first_indv, second_indv):
+        first_keys = (self._key_func(first_indv),
+                      self._key_func_2(first_indv))
+        second_keys = (self._key_func(second_indv),
+                       self._key_func_2(second_indv))
+        if first_keys[0] > second_keys[0] or first_keys[1] > second_keys[1]:
+            return False
+
+        not_equal = first_keys[0] != second_keys[0] or \
+            first_keys[1] != second_keys[1]
+        return not_equal
+
+    def _remove_dominated_pf_memebers(self, individual):
+        dominated_hof_members = self._get_dominated_hof_members(individual)
+        for i in reversed(dominated_hof_members):
+            self.remove(i)
+
+    def _get_dominated_hof_members(self, individual):
+        dominated_members = []
+        for i, hof_memeber in enumerate(self):
+            if self._first_dominates(individual, hof_memeber):
+                dominated_members.append(i)
+        return dominated_members
---
+++ tests/Base/test_pareto_front.py
@@ -0,0 +1,48 @@
+# Ignoring some linting rules in tests
+# pylint: disable=redefined-outer-name
+# pylint: disable=missing-docstring
+import pytest
+from collections import namedtuple
+
+from bingo.Base.ParetoFront import ParetoFront
+
+DummyIndv = namedtuple('DummyIndv', ['fitness', 'gene', 'att1', 'att2'])
+
+
+@pytest.fixture
+def empty_pf():
+    similar = lambda indv1, indv2 : indv1.gene == indv2.gene
+    key_2 = lambda indv : indv.att1
+    return ParetoFront(similarity_function=similar, secondary_key=key_2)
+
+
+@pytest.fixture
+def full_pf():
+    similar = lambda indv1, indv2 : indv1.gene == indv2.gene
+    key_2 = lambda indv : indv.att1
+    hof = ParetoFront(similarity_function=similar, secondary_key=key_2)
+    for i in range(5):
+        hof.insert(DummyIndv(i, i, 4 - i, 4 - i))
+    return hof
+
+
+@pytest.fixture(params=["empty", "full"])
+def all_pfs(request, empty_pf, full_pf):
+    if request.param == "empty":
+        return empty_pf
+    return full_pf
+
+@pytest.mark.parametrize("pop, new_len",
+                        [([DummyIndv(-1, -1, 5, 3)], 6),
+                         ([DummyIndv(-1, 0, 5, 3)], 5),
+                         ([DummyIndv(0, -1, 5, 3)], 5),
+                         ([DummyIndv(0, -1, 0, 3)], 1),
+                         ([DummyIndv(-1, -1, 5, 3),
+                           DummyIndv(-1, -2, -1, 3)], 1),
+                         ])
+def test_update_adds_indvs_propoerly(full_pf, pop, new_len):
+    full_pf.update(pop)
+    assert len(full_pf) == new_len
+
+
+

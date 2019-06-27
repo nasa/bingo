b1692dd28fb1648d880324d8ac40fa3836d61bf7
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2019-06-27 14:16:22 -0600

    updated parallel archipelago tests to have better messages when mpi4py is improperly installed

--- tests/Base/test_parallel_archipelago.py
+++ tests/Base/test_parallel_archipelago.py
@@ -12,7 +12,13 @@ from bingo.Base.MuPlusLambdaEA import MuPlusLambda
 from bingo.Base.TournamentSelection import Tournament
 from bingo.Base.Evaluation import Evaluation
 from bingo.Base.FitnessFunction import FitnessFunction
-from bingo.Base.ParallelArchipelago import ParallelArchipelago
+
+try:
+    from bingo.Base.ParallelArchipelago import ParallelArchipelago
+    PAR_ARCH_LOADED = True
+except ImportError:
+    CppBackend = None
+    PAR_ARCH_LOADED = False
 
 
 POP_SIZE = 5
@@ -100,6 +106,13 @@ def island(evol_alg):
     return Island(evol_alg, generator, POP_SIZE)
 
 
+def test_mpi4py_could_be_imported():
+    import mpi4py
+
+
+@pytest.mark.skipif(not PAR_ARCH_LOADED,
+                    reason="ParallelArchipelago import failure. "
+                           "Likely due to an import error of mpi4py.")
 def test_best_individual_returned(one_island):
     generator = MultipleValueChromosomeGenerator(generate_zero,
                                                  VALUE_LIST_SIZE)
@@ -109,6 +122,9 @@ def test_best_individual_returned(one_island):
     assert archipelago.get_best_individual().fitness == 0
 
 
+@pytest.mark.skipif(not PAR_ARCH_LOADED,
+                    reason="ParallelArchipelago import failure. "
+                           "Likely due to an import error of mpi4py.")
 def test_best_fitness_returned(one_island):
     generator = MultipleValueChromosomeGenerator(generate_zero,
                                                  VALUE_LIST_SIZE)
@@ -118,28 +134,19 @@ def test_best_fitness_returned(one_island):
     assert archipelago.get_best_fitness() == 0
 
 
+@pytest.mark.skipif(not PAR_ARCH_LOADED,
+                    reason="ParallelArchipelago import failure. "
+                           "Likely due to an import error of mpi4py.")
 def test_potential_hof_members(mocker, one_island):
     island_a = mocker.Mock(hall_of_fame=['a'])
     archipelago = ParallelArchipelago(one_island)
     archipelago._island = island_a
     assert archipelago._get_potential_hof_members() == ['a']
 
-# def test_island_migration(one_island, island_list):
-#     archipelago = SerialArchipelago(one_island, num_islands=4)
-#     archipelago._islands = island_list
-#
-#     archipelago._coordinate_migration_between_islands()
-#
-#     migration_count = 0
-#     for i, island in enumerate(archipelago._islands):
-#         initial_individual_values = [i]*VALUE_LIST_SIZE
-#         for individual in island.population:
-#             if initial_individual_values != individual.values:
-#                 migration_count += 1
-#                 break
-#     assert len(island_list) == migration_count
 
-
+@pytest.mark.skipif(not PAR_ARCH_LOADED,
+                    reason="ParallelArchipelago import failure. "
+                           "Likely due to an import error of mpi4py.")
 @pytest.mark.parametrize("sync_freq", [1, 10])
 @pytest.mark.parametrize("non_blocking", [True, False])
 def test_fitness_eval_count(one_island, sync_freq, non_blocking):
@@ -149,39 +156,8 @@ def test_fitness_eval_count(one_island, sync_freq, non_blocking):
     assert archipelago.get_fitness_evaluation_count() == 0
     archipelago.evolve(1)
     if non_blocking:
-        expected_evaluations = num_islands * (POP_SIZE +
+        expected_evals = num_islands * (POP_SIZE +
                                               sync_freq * OFFSPRING_SIZE)
     else:
-        expected_evaluations = num_islands * (POP_SIZE + OFFSPRING_SIZE)
-    assert archipelago.get_fitness_evaluation_count() == expected_evaluations
-
-
-# def test_archipelago_runs(one_island, two_island, three_island):
-#     max_generations = 100
-#     min_generations = 20
-#     error_tol = 0
-#     generation_step_report = 10
-#     archipelago = SerialArchipelago(one_island, num_islands=4)
-#     archipelago._islands = [one_island, two_island, three_island, three_island]
-#     result = archipelago.evolve_until_convergence(max_generations,
-#                                                   error_tol,
-#                                                   generation_step_report,
-#                                                   min_generations)
-#     assert result.success
-
-
-# def test_total_population_not_affected_by_migration(one_island):
-#     archipelago = SerialArchipelago(one_island, num_islands=4)
-#     total_pop_before = sum([len(i.population) for i in archipelago._islands])
-#     archipelago._coordinate_migration_between_islands()
-#     total_pop_after = sum([len(i.population) for i in archipelago._islands])
-#     assert total_pop_after == total_pop_before
-
-
-# def test_potential_hof_members(mocker, one_island):
-#     island_a = mocker.Mock(hall_of_fame=['a'])
-#     island_b = mocker.Mock(hall_of_fame=['b'])
-#     island_c = mocker.Mock(hall_of_fame=['c'])
-#     archipelago = SerialArchipelago(one_island, num_islands=3)
-#     archipelago._islands = [island_a, island_b, island_c]
-#     assert archipelago._get_potential_hof_members() == ['a', 'b', 'c']
+        expected_evals = num_islands * (POP_SIZE + OFFSPRING_SIZE)
+    assert archipelago.get_fitness_evaluation_count() == expected_evals

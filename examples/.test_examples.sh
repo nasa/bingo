79e85ce3948f2861f37edd2010c79b98978d0ade
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2019-10-16 14:21:40 -0600

    logging which backend is used for examples

--- examples/.test_examples.sh
+++ examples/.test_examples.sh
@@ -1,5 +1,7 @@
 set -e
 
+python -c "from bingo.symbolic_regression.agraph import agraph; print('Using %s Backend' % ('c++' if agraph.Backend.is_cpp() else 'Python'))"
+
 for i in examples/*.ipynb
 do
   echo "Running Notebook: $i"

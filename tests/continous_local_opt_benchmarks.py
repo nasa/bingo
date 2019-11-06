19b7686e0039f8b9aafe8eb6d906fe0055c1e99e
Tyler S. Townsend <ttown@knights.ucf.edu>
2019-11-06 12:44:30 -0500

    Added logging for iterations and use default benchmark data. AGraphs do not set constants when generated

--- tests/continous_local_opt_benchmarks.py
+++ tests/continous_local_opt_benchmarks.py
@@ -9,11 +9,16 @@ from bingo.local_optimizers.continuous_local_opt \
     import ContinuousLocalOptimization
 from bingocpp.build import bingocpp as cppBackend
 
+from tests.benchmark_data import StatsPrinter, \
+    generate_random_individuals, \
+    copy_to_cpp, \
+    TEST_EXPLICIT_REGRESSION, \
+    TEST_EXPLICIT_REGRESSION_CPP, \
+    TEST_IMPLICIT_REGRESSION, \
+    TEST_IMPLICIT_REGRESSION_CPP
 
-from tests.benchmark_data import StatsPrinter, TEST_AGRAPHS, TEST_AGRAPHS_CPP
-from tests.fitness_benchmark import TEST_EXPLICIT_REGRESSION, \
-                                    TEST_EXPLICIT_REGRESSION_CPP, \
-                                    TEST_IMPLICIT_REGRESSION, TEST_IMPLICIT_REGRESSION_CPP
+import tests.benchmark_data as benchmark_data
+
 
 TEST_EXPLICIT_REGRESSION_OPTIMIZATION \
     = ContinuousLocalOptimization(TEST_EXPLICIT_REGRESSION)
@@ -25,53 +30,70 @@ TEST_IMPLICIT_REGRESSION_OPTIMIZATION_CPP \
     = ContinuousLocalOptimization(TEST_IMPLICIT_REGRESSION_CPP)
 
 
-TEST_AGRAPH_LISTS = []
-TEST_AGRAPH_LISTS_CPP = []
+TEST_ITERATION = 0
+DEBUG = False
+TEST_AGRAPHS = generate_random_individuals(benchmark_data.NUM_AGRAPHS_INDVS,
+                                           benchmark_data.COMMAND_ARRAY_SIZE,
+                                           True)
+TEST_AGRAPHS_CPP = copy_to_cpp(TEST_AGRAPHS)
+BENCHMARK_LISTS = []
+BENCHMARK_LISTS_CPP = []
 
 
 def benchmark_explicit_regression_with_optimization():
-    for test_run in TEST_AGRAPH_LISTS:
+    for i, test_run in enumerate(BENCHMARK_LISTS):
         for indv in test_run:
             _ = TEST_EXPLICIT_REGRESSION_OPTIMIZATION.__call__(indv)
 
 
 def benchmark_explicit_regression_cpp_with_optimization():
-    for test_run in TEST_AGRAPH_LISTS_CPP:
+    for i, test_run in enumerate(BENCHMARK_LISTS_CPP):
         for indv in test_run:
             _ = TEST_EXPLICIT_REGRESSION_OPTIMIZATION_CPP.__call__(indv)
 
 
 def benchmark_implicit_regression_with_optimization():
-    for test_run in TEST_AGRAPH_LISTS:
+    for i, test_run in enumerate(BENCHMARK_LISTS):
         for indv in test_run:
-            _ = TEST_IMPLICIT_REGRESSION.__call__(indv)
+            _ = TEST_IMPLICIT_REGRESSION_OPTIMIZATION.__call__(indv)
 
 
 def benchmark_implicit_regression_cpp_with_optimization():
-    for test_run in TEST_AGRAPH_LISTS_CPP:
+    for i, test_run in enumerate(BENCHMARK_LISTS_CPP):
         for indv in test_run:
-            _ = TEST_IMPLICIT_REGRESSION_CPP.__call__(indv)
+            _ = TEST_IMPLICIT_REGRESSION_OPTIMIZATION_CPP.__call__(indv)
 
 
 def reset_test_data():
-    TEST_AGRAPH_LISTS.clear()
-    for num_runs in range(0, 100):
-        run_list = []
-        for agraph in TEST_AGRAPHS:
-            run_list.append(agraph.copy())
-        TEST_AGRAPH_LISTS.append(run_list)
+    _reset_test_data_helper(TEST_AGRAPHS, BENCHMARK_LISTS)
 
 
 def reset_test_data_cpp():
-    TEST_AGRAPH_LISTS_CPP.clear()
-    for num_runs in range(0, 100):
+    _reset_test_data_helper(TEST_AGRAPHS_CPP, BENCHMARK_LISTS_CPP)
+
+
+def _reset_test_data_helper(agraph_list, benchmarking_array):
+    global TEST_ITERATION
+    if DEBUG:
+        print("...Executing iteration:", TEST_ITERATION)
+    _create_fresh_benchmarking_array(agraph_list, benchmarking_array)
+    TEST_ITERATION += 1
+
+
+def _create_fresh_benchmarking_array(agraph_list, benchmarking_array):
+    benchmarking_array.clear()
+    for num_runs in range(0, benchmark_data.BENCHMARK_EVALUATION_COUNT):
         run_list = []
-        for agraph in TEST_AGRAPHS_CPP:
+        for agraph in agraph_list:
             run_list.append(agraph.copy())
-        TEST_AGRAPH_LISTS_CPP.append(run_list)
+        benchmarking_array.append(run_list)
 
 
-def do_benchmarking():
+def do_benchmarking(debug = False):
+    if debug:
+        global DEBUG
+        DEBUG = True
+
     benchmarks = [
         [benchmark_explicit_regression_with_optimization,
          benchmark_explicit_regression_cpp_with_optimization,
@@ -92,12 +114,38 @@ def do_benchmarking():
 def _run_benchmarks(printer, regression, regression_cpp):
     for backend, name in [[pyBackend, " py"], [cppBackend, "c++"]]:
         agraph_module.Backend = backend
-        printer.add_stats("py:  fitness " +name + ": evaluate ",
-                          timeit.repeat(regression, setup=reset_test_data,
-                                        number=1, repeat=10))
-    printer.add_stats("c++: fitness c++: evaluate ",
-                      timeit.repeat(regression_cpp, setup=reset_test_data_cpp,
-                                    number=1, repeat=10))
+        _add_stats_and_log_intermediate_steps(
+            printer,
+            regression,
+            "py:  fitness " +name + ": evaluate ",
+            reset_test_data
+        )
+    _add_stats_and_log_intermediate_steps(
+        printer,
+        regression_cpp,
+        "c++: fitness c++: evaluate ",
+        reset_test_data_cpp
+    )
+
+def _add_stats_and_log_intermediate_steps(stats_printer,
+                                          regression, 
+                                          run_name,
+                                          setup_function):
+    if DEBUG:
+        print("Running", regression.__name__, "...................")
+    stats_printer.add_stats(
+        run_name,
+        timeit.repeat(regression, setup=setup_function, number=1,
+                      repeat=benchmark_data.BENCHMARK_DATA_POINTS)
+    )
+    if DEBUG:
+        print(regression.__name__, "finished\n")
+    _reset_iteration_count()
+
+
+def _reset_iteration_count():
+    global TEST_ITERATION
+    TEST_ITERATION = 0
 
 
 def _print_stats(printer_list):

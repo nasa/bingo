fb5fdca605adf574388ce78fc5e18eb77b8669a8
Tyler S. Townsend <ttown@knights.ucf.edu>
2019-11-06 12:45:28 -0500

    Added fitness benchmarks and optimzation benchmarks.

--- tests/performance_benchmarks.py
+++ tests/performance_benchmarks.py
@@ -2,6 +2,11 @@
 # pylint: disable=redefined-outer-name
 # pylint: disable=missing-docstring
 import tests.evaluation_benchmark as evaluation_benchmark
+import tests.fitness_benchmark as fitness_benchmark
+import tests.continous_local_opt_benchmarks as clo_benchmark
 
 if __name__ == '__main__':
     evaluation_benchmark.do_benchmarking()
+    fitness_benchmark.do_benchmarking()
+    clo_benchmark.do_benchmarking(debug=True)
+

a0c8a843b56b0b422cb0c59728113c6452d2e952
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2021-06-16 15:37:06 -0600

    fixing imports for symbolic regression

--- bingo/symbolic_regression/__init__.py
+++ bingo/symbolic_regression/__init__.py
@@ -4,23 +4,28 @@ Import the core names of bingo symbolic_regression library
 Programs that want to build bingo symbolic regression apps
 without having to import specific modules can import this.
 """
+import warnings
 
-from .agraph.component_generator import *
-from .agraph.crossover import *
-from .agraph.generator import *
-from .agraph.mutation import *
-from .atomic_potential_regression import *
+from .agraph.component_generator import ComponentGenerator
+from .agraph.crossover import AGraphCrossover
+from .agraph.generator import AGraphGenerator
+from .agraph.mutation import AGraphMutation
+from .atomic_potential_regression import PairwiseAtomicPotential, \
+                                         PairwiseAtomicTrainingData
 
 # Try to load in C++ cpython extensions
 # TODO: consider this init file but remove imports for python
 # that have C++ bindings
 try:
-    from bingocpp.symbolic_regression import *
+    from bingocpp import AGraph, \
+                         ExplicitRegression, \
+                         ExplicitTrainingData, \
+                         ImplicitRegression, \
+                         ImplicitTrainingData
     ISCPP = True
-except ImportError as e:
-    from .agraph.agraph import *
-    from .equation import *
-    from .explicit_regression import *
-    from .implicit_regression import *
+except ImportError as import_err:
+    from .agraph.agraph import AGraph
+    from .explicit_regression import ExplicitRegression, ExplicitTrainingData
+    from .implicit_regression import ImplicitRegression, ImplicitTrainingData
     ISCPP = False
-    print("Could not load C++ modules")
+    warnings.warn(f"Could not load C++ modules {import_err}")

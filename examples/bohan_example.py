3c7d56ac45c288bf93423358733f8185c452bedd
Li <bli6@ndc.nasa.gov>
2021-06-28 00:49:37 -0400

    added all ops

--- examples/bohan_example.py
+++ examples/bohan_example.py
@@ -23,9 +23,9 @@ def _evaluate_from_np(graph, data, constants):
 
 def _create_random_equation():
     comp_gen = ComponentGenerator(2)
-    for op in ["+", "-", "*", "/",
+    for op in ["+", "-", "*", "/", "|",
                "sin", "cos", "sinh", "cosh",
-               "log", "sqrt", "pow", "exp", "abs"]:
+               "log", "sqrt", "pow", "exp"]:
         comp_gen.add_operator(op)
     equ_gen = AGraphGenerator(32, comp_gen, use_simplification=True)
 

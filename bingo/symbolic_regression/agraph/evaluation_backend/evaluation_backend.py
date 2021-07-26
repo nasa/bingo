c363e47415278d8b2a06ab21c9fbf6e7593a5cb1
Li <bli6@ndc.nasa.gov>
2021-07-25 23:10:32 -0400

    removed print

--- bingo/symbolic_regression/agraph/evaluation_backend/evaluation_backend.py
+++ bingo/symbolic_regression/agraph/evaluation_backend/evaluation_backend.py
@@ -44,7 +44,6 @@ def evaluate(stack, x, constants, use_gpu = False):
         if hasattr(constants, 'shape'):
             num_particles = constants.shape[1]
 
-        print(f"particle count {num_particles}")
         forward_eval = cp.ones((len(stack), x.shape[0], num_particles)) * np.inf
         blockspergrid = math.ceil(x.shape[0] * num_particles / gi.GPU_THREADS_PER_BLOCK)
         _f_eval_gpu_kernel[blockspergrid, gi.GPU_THREADS_PER_BLOCK](stack, x, constants, num_particles, x.shape[0], stack.shape[0], forward_eval)

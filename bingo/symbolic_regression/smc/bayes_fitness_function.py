70ff781a1eb719a18058347fdb9fde59e1ff68c9
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2021-09-02 08:39:43 -0600

    updating bff for multistarts

--- bingo/symbolic_regression/smc/bayes_fitness_function.py
+++ bingo/symbolic_regression/smc/bayes_fitness_function.py
@@ -14,7 +14,9 @@ from smcpy import ImproperUniform
 class BayesFitnessFunction(FitnessFunction):
 
     def __init__(self, continuous_local_opt, num_particles=150, phi_exponent=8,
-                 smc_steps=15, mcmc_steps=12, ess_threshold=0.75, std=None):
+                 smc_steps=15, mcmc_steps=12, ess_threshold=0.75, std=None,
+                 return_nmll_only=True, num_multistarts=1,
+                 uniformly_weighted_proposal=True):
 
         if smc_steps <= 2:
             raise ValueError('smc_steps must be > 2')
@@ -26,6 +28,9 @@ class BayesFitnessFunction(FitnessFunction):
         self._mcmc_steps = mcmc_steps
         self._ess_threshold = ess_threshold
         self._std = std
+        self._return_nmll_only = return_nmll_only
+        self._num_multistarts = num_multistarts
+        self._uniformly_weighted_proposal = uniformly_weighted_proposal
 
         self._num_observations = len(continuous_local_opt.training_data.x)
         self._cont_local_opt = continuous_local_opt
@@ -38,8 +43,10 @@ class BayesFitnessFunction(FitnessFunction):
         try:
             proposal = self.generate_proposal_samples(individual,
                                                       self._num_particles)
-        except ValueError:
-            return np.nan
+        except (ValueError, np.linalg.LinAlgError) as e:
+            if self._return_nmll_only:
+                return np.nan
+            return np.nan, None, None
 
         priors = [ImproperUniform() for _ in range(len(param_names))]
         if self._std is None:
@@ -48,24 +55,36 @@ class BayesFitnessFunction(FitnessFunction):
 
         vector_mcmc = VectorMCMC(lambda x: self.evaluate_model(x, individual),
                                  self.training_data.y.flatten(), priors,
-                                 std_dev=self._std)
+                                 log_like_args=self._std)
 
         mcmc_kernel = VectorMCMCKernel(vector_mcmc, param_order=param_names)
         smc = SMCSampler(mcmc_kernel)
 
-        step_list, marginal_log_likes = smc.sample(self._num_particles,
-                                                   self._mcmc_steps,
-                                                   self._phi_seq,
-                                                   self._ess_threshold,
-                                                   proposal)
+        try:
+            step_list, marginal_log_likes = smc.sample(self._num_particles,
+                                                       self._mcmc_steps,
+                                                       self._phi_seq,
+                                                       self._ess_threshold,
+                                                       proposal)
+        except (ValueError, np.linalg.LinAlgError, ZeroDivisionError):
+            if self._return_nmll_only:
+                return np.nan
+            return np.nan, None, None
 
-        means = step_list[-1].compute_mean(package=False)
-        individual.set_local_optimization_params(means)
+        # means = step_list[-1].compute_mean(package=False)
+        max_idx = np.argmax(step_list[-1].log_likes)
+        maps = step_list[-1].params[max_idx]
+        individual.set_local_optimization_params(maps[:-1])
 
-        return -1 * (marginal_log_likes[-1]
-                     - marginal_log_likes[self._fbf_phi_idx])
+        nmll = -1 * (marginal_log_likes[-1] -
+                     marginal_log_likes[self._fbf_phi_idx])
 
-    def get_parameter_names(self, individual):
+        if self._return_nmll_only:
+            return nmll
+        return nmll, step_list, vector_mcmc
+
+    @staticmethod
+    def get_parameter_names(individual):
         num_params = individual.get_number_local_optimization_params()
         return [f'p{i}' for i in range(num_params)]
 
@@ -76,37 +95,60 @@ class BayesFitnessFunction(FitnessFunction):
         return individual
 
     def estimate_covariance(self, individual):
+        self.do_local_opt(individual)
         num_params = individual.get_number_local_optimization_params()
         x = self.training_data.x
         f, f_deriv = individual.evaluate_equation_with_local_opt_gradient_at(x)
         ssqe = np.sum((self.training_data.y - f) ** 2)
         var_ols = ssqe / (len(f) - num_params)
-        cov = var_ols * f_deriv.T.dot(f_deriv)
-        return cov, var_ols, ssqe
+        cov = var_ols * np.linalg.inv(f_deriv.T.dot(f_deriv))
+        return individual.constants, cov, var_ols, ssqe
 
     def generate_proposal_samples(self, individual, num_samples):
         param_names = self.get_parameter_names(individual)
         pdf = np.ones((num_samples, 1))
         samples = np.ones((num_samples, len(param_names)))
 
-        cov, var_ols, ssqe = self.estimate_covariance(individual)
+        num_multistarts = self._num_multistarts
+        if param_names == []:
+            num_multistarts = 1
+
+        cov_estimates = [self.estimate_covariance(individual)
+                         for _ in range(num_multistarts)]
+
         if param_names != []:
-            proposal = mvn(individual.constants, cov, allow_singular=True)
-            samples = proposal.rvs(num_samples).reshape(num_samples, -1)
-            pdf *= proposal.pdf(samples).reshape(-1, 1)
+            param_dists = [mvn(mean, cov, allow_singular=True)
+                           for mean, cov, _, _ in cov_estimates]
+            pdf, samples = self._get_samples_and_pdf(param_dists, num_samples)
 
         if self._std is None:
             len_data = len(self.training_data.x)
-            noise_proposal = invgamma((0.01 + len_data) / 2,
-                                      scale=(0.01 * var_ols + ssqe) / 2)
-            noise_samples = noise_proposal.rvs(num_samples).reshape(-1, 1)
-            pdf *= noise_proposal.pdf(noise_samples).reshape(-1, 1)
+            noise_dists = [invgamma((0.01 + len_data) / 2,
+                                    scale=(0.01 * var_ols + ssqe) / 2)
+                           for _, _, var_ols, ssqe in cov_estimates]
+            noise_pdf, noise_samples = self._get_samples_and_pdf(noise_dists,
+                                                                 num_samples)
 
             param_names.append('std_dev')
             samples = np.concatenate((samples, noise_samples), axis=1)
+            pdf *= noise_pdf
+
+        if self._uniformly_weighted_proposal:
+            pdf = np.ones_like(pdf)
 
         samples = dict(zip(param_names, samples.T))
         return samples, pdf
+
+    @staticmethod
+    def _get_samples_and_pdf(distributions, num_samples):
+        sub_samples = num_samples // len(distributions)
+        samples = np.vstack([proposal.rvs(sub_samples).reshape(sub_samples, -1)
+                             for proposal in distributions])
+        pdf = np.zeros((samples.shape[0], 1))
+        for dist in distributions:
+            pdf += dist.pdf(samples).reshape(-1, 1)
+        pdf /= len(distributions)
+        return pdf, samples
 
     def evaluate_model(self, params, individual):
         self._eval_count += 1

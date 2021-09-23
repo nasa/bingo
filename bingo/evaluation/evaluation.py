0df25187985cdb4316f24e8bdbde6b00924ba4ff
Geoffrey Bomarito <geoffrey.f.bomarito@nasa.gov>
2021-09-23 14:54:39 -0600

    replacing joblib parallel with multiprocessing

--- bingo/evaluation/evaluation.py
+++ bingo/evaluation/evaluation.py
@@ -3,13 +3,7 @@
 This module defines the a basic form of the evaluation phase of bingo
 evolutionary algorithms.
 """
-
-import multiprocessing
-
-import bingo.util.global_imports as gi
-
-num_cores = multiprocessing.cpu_count()
-print(f"number of cores: {num_cores}")
+from multiprocessing import Pool
 
 class Evaluation:
     """Base phase for calculating fitness of a population.
@@ -24,6 +18,9 @@ class Evaluation:
     fitness_function : FitnessFunction
         The function class that is used to calculate fitnesses of individuals
         in the population.
+    redundant : bool
+        Whether to re-evaluate individuals that have been evaluated previously.
+        Default False.
 
     Attributes
     ----------
@@ -33,8 +30,10 @@ class Evaluation:
     eval_count : int
         the number of fitness function evaluations that have occurred
     """
-    def __init__(self, fitness_function):
+    def __init__(self, fitness_function, redundant=False, multiprocess=False):
         self.fitness_function = fitness_function
+        self._redundant = redundant
+        self._multiprocess = multiprocess
 
     @property
     def eval_count(self):
@@ -53,21 +52,36 @@ class Evaluation:
         population : list of chromosomes
                      population for which fitness should be calculated
         """
-
-        def eval_fitness(indv):
-            if not indv.fit_set:
-                return self.fitness_function(indv)
-
-        if gi.USING_PARALLEL_CPU:
-            fitnesses = gi.jl.Parallel(n_jobs=num_cores)(gi.jl.delayed(eval_fitness)(indv) for indv in population)
-
-            for i, indv in enumerate(population):
-                if not indv.fit_set:
-                    indv.fitness = fitnesses[i]
-
+        if self._multiprocess:
+            self._multiprocess_eval(population)
         else:
-            for indv in population:
-                if not indv.fit_set:
-                    indv.fitness = self.fitness_function(indv)
+            self._serial_eval(population)
+
+    def _serial_eval(self, population):
+        for indv in population:
+            if self._redundant or not indv.fit_set:
+                indv.fitness = self.fitness_function(indv)
+
+    def _multiprocess_eval(self, population):
+        num_procs = self._multiprocess if isinstance(self._multiprocess, int) \
+            else None
+
+        with Pool(processes=num_procs) as pool:
+            results = []
+            for i, indv in enumerate(population):
+                if self._redundant or not indv.fit_set:
+                    results.append(
+                            pool.apply_async(_fitness_job,
+                                             (indv, self.fitness_function, i)))
+
+            for res in results:
+                indv, i = res.get()
+                population[i] = indv
+
+
+def _fitness_job(individual, fitness_function, population_index):
+    individual.fitness = fitness_function(individual)
+    return individual, population_index
+
 
 

dd6fecbe41b456ca3bf58b6dd7dc12a54111372d
David Randall <madmathskills@gmail.com>
2022-01-25 14:49:01 -0700

    bug fixes and todos

--- bingo/symbolic_regression/agraph/string_parsing.py
+++ bingo/symbolic_regression/agraph/string_parsing.py
@@ -15,6 +15,7 @@ operator_map = {"+": ADDITION, "-": SUBTRACTION, "*": MULTIPLICATION,
 var_or_const_pattern = re.compile(r"([XC])_(\d+)")  # matches X_### and C_###
 int_pattern = re.compile(r"\d+")  # matches ###
 non_unary_op_pattern = re.compile(r"([*/^()])")  # matches *, /, ^, (, or )
+negative_pattern = re.compile(r"-([^\s]+)")  # matches -XXXX
 
 
 def infix_to_postfix(infix_tokens):
@@ -167,6 +168,11 @@ def sympy_string_to_command_array_and_constants(sympy_string):
         A command array and list of constants
         corresponding to the expression given by sympy_string
     """
+    # TODO throw error w/ zoo
+    # TODO test -X_0 and zoo
+    # if "zoo" in sympy_string:
+    #     return np.array([[CONSTANT, 0, 0]], dtype=int), [0.0]
+    sympy_string = negative_pattern.sub(r"-1.0 * \1", sympy_string)
     infix_tokens = sympy_string_to_infix_tokens(sympy_string)
     postfix_tokens = infix_to_postfix(infix_tokens)
     return postfix_to_command_array_and_constants(postfix_tokens)

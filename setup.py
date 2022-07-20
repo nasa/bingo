d0d7699980f324b0ca1cf84390aaa52c796edcf3
David L. Randall <david.l.randall-1@nasa.gov>
2022-07-20 17:22:20 -0600

    updating module name for test

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ with open("README.md", "r") as fh:
 
 
 setup(
-    name="bingo-nasa",
+    name="bingo-nasa-test",
     version=get_property('__version__', 'bingo'),
     author="Geoffrey Bomarito",
     author_email="geoffrey.f.bomarito@nasa.gov",

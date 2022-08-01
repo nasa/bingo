b6f8aa3b71b9fbb97798fc1eead2c1cda4bb66ac
gbomarito <geoffrey.f.bomarito@nasa.gov>
2022-08-01 09:28:55 -0600

    Update .run_tests.sh

--- tests/.run_tests.sh
+++ tests/.run_tests.sh
@@ -20,11 +20,11 @@ echo "Running tests in $RUN_MODE mode"
 
 if [ $RUN_MODE == "coverage" ]
 then
-    $MPI_EXEC -np 3 coverage run --parallel-mode --source=bingo tests/integration/mpitest_parallel_archipelago.py
+    $MPI_EXEC -np 3 coverage run --parallel-mode --source=bingo tests/integration/mpitests/mpitest_parallel_archipelago.py
     coverage combine
     pytest tests --cov=bingo --cov-report=term-missing --cov-append
 elif [ $RUN_MODE == "normal" ]
 then
-    $MPI_EXEC -np 3 python tests/integration/mpitest_parallel_archipelago.py
+    $MPI_EXEC -np 3 python tests/integration/mpitests/mpitest_parallel_archipelago.py
     pytest tests
 fi

e9c0f83958b28e08b3f28e506a7d35725e8a3008
David L. Randall <david.l.randall-1@nasa.gov>
2022-08-01 10:27:08 -0600

    added functionality to run mpitests in normal mode

--- tests/.run_tests.sh
+++ tests/.run_tests.sh
@@ -4,15 +4,23 @@ set -e
 
 MPI_EXEC=$(python -c "import mpi4py;import os;filename = next(iter(mpi4py.get_config().items()))[1];print(os.path.dirname(filename)+'/mpiexec');")
 
-for i in tests/integration/mpitests/*.py
-do
-  if [ $i != "tests/integration/mpitests/mpitest_util.py" ]
-  then
-    echo "Running mpitest: $i"
-    $MPI_EXEC -np 3 coverage run --parallel-mode --source=bingo $i
-  fi
-done
-coverage combine
+run_mpi_tests() {  # $1 = run mode
+  for i in tests/integration/mpitests/*.py
+  do
+    if [ $i != "tests/integration/mpitests/mpitest_util.py" ]
+    then
+      echo "Running mpitest: $i in $1 mode"
+      if [ $1 == "coverage" ]
+      then
+        $MPI_EXEC -np 3 coverage run --parallel-mode --source=bingo $i
+      elif [ $1 == "normal" ]
+      then
+        $MPI_EXEC -np 3 python $i
+      fi
+    fi
+  done
+}
+
 
 RUN_MODE=${1-"coverage"}
 
@@ -20,11 +28,11 @@ echo "Running tests in $RUN_MODE mode"
 
 if [ $RUN_MODE == "coverage" ]
 then
-    $MPI_EXEC -np 3 coverage run --parallel-mode --source=bingo tests/integration/mpitests/mpitest_parallel_archipelago.py
-    coverage combine
-    pytest tests --cov=bingo --cov-report=term-missing --cov-append
+  run_mpi_tests $RUN_MODE
+  coverage combine
+  pytest tests --cov=bingo --cov-report=term-missing --cov-append
 elif [ $RUN_MODE == "normal" ]
 then
-    $MPI_EXEC -np 3 python tests/integration/mpitests/mpitest_parallel_archipelago.py
-    pytest tests
+  run_mpi_tests $RUN_MODE
+  pytest tests
 fi

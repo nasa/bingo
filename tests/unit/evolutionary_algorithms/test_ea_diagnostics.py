55f7f57f7c6c5a40db31380038ce8bac2b1a1c9f
David L. Randall <david.l.randall-1@nasa.gov>
2022-08-04 18:24:25 -0600

    added typed summary test

--- tests/unit/evolutionary_algorithms/test_ea_diagnostics.py
+++ tests/unit/evolutionary_algorithms/test_ea_diagnostics.py
@@ -26,29 +26,58 @@ def population_0123_times_4(dummy_chromosome):
     return [dummy_chromosome(fitness=i) for i in [0, 1, 2, 3] * 4]
 
 
-@pytest.mark.parametrize("crossover_type, mutation_type",
-                         [("normal_c", 1), ("normal_m", 1)])
-def test_correctly_updated_summary(population_12, population_0123_times_4,
-                                   crossover_type, mutation_type):
-    offspring_parents = [[0, 1]] * 8 + [[0]] * 6 + [[]]*2
+def test_correctly_updated_overall_summary(population_12,
+                                           population_0123_times_4):
+    offspring_parents = [[0, 1]] * 8 + [[0]] * 6 + [[]] * 2
     offspring_crossover_type = \
-        np.array([crossover_type] * 8 + [None] * 8, dtype=object)
+        np.array(["normal_c"] * 8 + [None] * 8, dtype=object)
     offspring_mutation_type = \
-        np.array([None] * 4 + [mutation_type] * 8 + [None] * 4, dtype=object)
+        np.array([None] * 4 + ["normal_m"] * 8 + [None] * 4, dtype=object)
 
     ead = EaDiagnostics()
     ead.update(population_12, population_0123_times_4, offspring_parents,
                offspring_crossover_type, offspring_mutation_type)
 
     expected_summary = EaDiagnosticsSummary(
-            beneficial_crossover_rate=0.25,
-            detrimental_crossover_rate=0.25,
-            beneficial_mutation_rate=0.25,
-            detrimental_mutation_rate=0.5,
-            beneficial_crossover_mutation_rate=0.25,
-            detrimental_crossover_mutation_rate=0.25)
+        beneficial_crossover_rate=0.25,
+        detrimental_crossover_rate=0.25,
+        beneficial_mutation_rate=0.25,
+        detrimental_mutation_rate=0.5,
+        beneficial_crossover_mutation_rate=0.25,
+        detrimental_crossover_mutation_rate=0.25)
 
     assert ead.summary == expected_summary
+
+
+def test_correctly_updated_type_summaries(population_12,
+                                         population_0123_times_4):
+    offspring_parents = [[0, 1]] * 16
+    offspring_crossover_type = \
+        np.array(["c_n"] * 2 + ["c_s"] * 4 + ["c_n"] * 4 + [None] * 6,
+                 dtype=object)
+    offspring_mutation_type = \
+        np.array([None] * 4 + ["m_n", "m_s"] * 5 + [None] * 2, dtype=object)
+
+    ead = EaDiagnostics()
+    ead.update(population_12, population_0123_times_4, offspring_parents,
+               offspring_crossover_type, offspring_mutation_type)
+
+    expected_cross_summary = {"c_n": (0.5, 0),
+                              "c_s": (0, 0.5)}
+
+    assert ead.crossover_type_summary == expected_cross_summary
+
+    expected_mut_summary = {"m_n": (0.5, 0),
+                            "m_s": (0, 0.5)}
+
+    assert ead.mutation_type_summary == expected_mut_summary
+
+    expected_cross_mut_summary = {"c_n": {"m_n": (0.5, 0),
+                                          "m_s": (0, 0.5)},
+                                  "c_S": {"m_n": (1, 0),
+                                          "m_s": (0, 0)}}
+
+    assert ead.crossover_mutation_summary == expected_cross_mut_summary
 
 
 @pytest.mark.parametrize("num_subsets", [1, 2, 4, 8])

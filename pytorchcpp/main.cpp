be552003c8adb7ef77c982c320432c1572ffd837
David Randall <madmathskills@gmail.com>
2023-07-17 03:40:54 -0600

    added updated pytorchcpp files

--- pytorchcpp/main.cpp
+++ pytorchcpp/main.cpp
@@ -15,6 +15,10 @@ typedef torch::Tensor (
     int, int, const torch::Tensor &,
     const torch::Tensor &, std::vector<torch::Tensor> &
 );
+torch::Tensor torch_loadint (int param1, int param2,
+                           const torch::Tensor &X, const torch::Tensor &constants,  std::vector<torch::Tensor> &stack) {
+    return torch::ones({X.size(1), 1}, torch::dtype(torch::kFloat64)) * param1;
+}
 torch::Tensor torch_loadx (int param1, int param2,
         const torch::Tensor &X, const torch::Tensor &constants,  std::vector<torch::Tensor> &stack) {
     return X[param1].view({X.size(1), 1});
@@ -80,20 +84,39 @@ torch::Tensor torch_sqrt (int param1, int param2,
     return torch::sqrt(torch::abs(stack[param1]));
 }
 
+torch::Tensor torch_safe_power (int param1, int param2,
+        const torch::Tensor &X, const torch::Tensor &constants,  std::vector<torch::Tensor> &stack) {
+    return torch::pow(torch::abs(stack[param1]), stack[param2]);
+}
+
+torch::Tensor torch_sinh (int param1, int param2,
+                                const torch::Tensor &X, const torch::Tensor &constants,  std::vector<torch::Tensor> &stack) {
+  return torch::sinh(stack[param1]);
+}
+
+torch::Tensor torch_cosh (int param1, int param2,
+                                const torch::Tensor &X, const torch::Tensor &constants,  std::vector<torch::Tensor> &stack) {
+  return torch::cosh(stack[param1]);
+}
+
 const torch_op_fn operator_fns[] = {
-  torch_loadx,  // 0
-  torch_loadc,  // 1
-  torch_add,  // 2
-  torch_subtract,  // 3
-  torch_multiply,  // 4
-  torch_divide,  // 5
-  torch_sin,  // 6
-  torch_cos,  // 7
-  torch_exp,  // 8
-  torch_log,  // 9
-  torch_pow,  // 10
-  torch_abs,  // 11
-  torch_sqrt  // 12
+    torch_loadint, // -1
+    torch_loadx,  // 0
+    torch_loadc,  // 1
+    torch_add,  // 2
+    torch_subtract,  // 3
+    torch_multiply,  // 4
+    torch_divide,  // 5
+    torch_sin,  // 6
+    torch_cos,  // 7
+    torch_exp,  // 8
+    torch_log,  // 9
+    torch_pow,  // 10
+    torch_abs,  // 11
+    torch_sqrt,  // 12
+    torch_safe_power,  // 13
+    torch_sinh,  // 14
+    torch_cosh,  // 15
 };
 
 
@@ -106,7 +129,7 @@ torch::Tensor get_forward_eval(
 
     std::vector<torch::Tensor> forward_eval(stack_size);
     for (int i = 0; i < stack_size; i++) {
-        forward_eval[i] = (*operator_fns[stack_info(i, 0)])(stack_info(i, 1), stack_info(i, 2), X, constants, forward_eval);
+        forward_eval[i] = (*operator_fns[stack_info(i, 0) + 1])(stack_info(i, 1), stack_info(i, 2), X, constants, forward_eval);
     }
 
     return forward_eval[stack_size - 1];
@@ -119,23 +142,20 @@ torch::autograd::tensor_list eval_with_deriv(
     bool wrt_x_or_c) {
     pybind11::gil_scoped_release no_gil;
 
-    torch::Tensor eval_constants = constants;
-
     torch::autograd::tensor_list inputs = {X};
     if (!wrt_x_or_c) {
-//        eval_constants = eval_constants.view({eval_constants.size(0), 1}) * torch::ones_like(X);
-        inputs = {eval_constants};
+        inputs = {constants};
     }
     inputs[0].set_requires_grad(true);
 
-    torch::Tensor eval = get_forward_eval(stack, X, eval_constants);
+    torch::Tensor eval = get_forward_eval(stack, X, constants);
 
     torch::Tensor deriv;
     if (eval.requires_grad()) {
         deriv = torch::autograd::grad({eval}, inputs, {torch::ones_like(eval)}, c10::nullopt, false, false)[0];
     }
     else {
-        deriv = torch::zeros_like(eval);
+        deriv = torch::zeros({inputs[0].size(0), eval.size(0)});
     }
     inputs[0].set_requires_grad(false);
 

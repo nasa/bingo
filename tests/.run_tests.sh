45953911e6ff1e9b17605da0934a530416a45722
G. Bomarito <geoffrey.f.bomarito@nasa.gov>
2024-09-23 16:04:49 -0400

    trying to run without coverage

--- tests/.run_tests.sh
+++ tests/.run_tests.sh
@@ -10,7 +10,7 @@ then
 fi
 echo "found $MPI_EXEC"
 
-RUN_MODE=${1-"coverage"}
+RUN_MODE=${1-"normal"}
 echo "Running tests in $RUN_MODE mode"
 
 # run mpi tests
@@ -21,10 +21,10 @@ do
     echo "Running mpitest: $i in $RUN_MODE mode"
     if [ $RUN_MODE == "coverage" ]
     then
-      $MPI_EXEC -np 2 coverage run --parallel-mode --source=bingo $i
+      $MPI_EXEC -n 2 coverage run --parallel-mode --source=bingo $i
     elif [ $RUN_MODE == "normal" ]
     then
-      $MPI_EXEC -np 2 python $i
+      $MPI_EXEC -n 2 python $i
     fi
   fi
 done

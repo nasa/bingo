099f25ca75826952eb2e836ea51c11613e74fa03
G. Bomarito <geoffrey.f.bomarito@nasa.gov>
2024-09-24 10:50:17 -0400

    adding smcpy to pip build

--- setup.py
+++ setup.py
@@ -11,13 +11,15 @@ from setuptools.command.build_ext import build_ext
 
 
 def get_property(prop, project):
-    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
-                       open(project + '/__init__.py').read())
+    result = re.search(
+        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+        open(project + "/__init__.py").read(),
+    )
     return result.group(1)
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
+    def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
@@ -25,17 +27,19 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            out = subprocess.check_output(["cmake", "--version"])
         except OSError:
             warnings.warn(
-                "CMake must be installed to build the following extensions: " +
-                ", ".join(e.name for e in self.extensions))
+                "CMake must be installed to build the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
             return
 
         if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-                                         out.decode()).group(1))
-            if cmake_version < '3.1.0':
+            cmake_version = LooseVersion(
+                re.search(r"version\s*([\d.]+)", out.decode()).group(1)
+            )
+            if cmake_version < "3.1.0":
                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
         try:
@@ -45,36 +49,39 @@ class CMakeBuild(build_ext):
             warnings.warn("Couldn't install bingocpp")
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+        ]
 
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                cfg.upper(),
-                extdir)]
+            cmake_args += [
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+            ]
             if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
+                cmake_args += ["-A", "x64"]
+            build_args += ["--", "/m"]
         else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+            build_args += ["--", "-j2"]
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get('CXXFLAGS', ''),
-            self.distribution.get_version())
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get("CXXFLAGS", ""), self.distribution.get_version()
+        )
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-                              cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.', '--target', 'bingocpp']
-                              + build_args,
-                              cwd=self.build_temp)
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
+        )
+        subprocess.check_call(
+            ["cmake", "--build", ".", "--target", "bingocpp"] + build_args,
+            cwd=self.build_temp,
+        )
         print()  # Add an empty line for cleaner output
 
 
@@ -84,49 +91,53 @@ with open("README.md", "r") as fh:
 
 setup(
     name="bingo-nasa",
-    version=get_property('__version__', 'bingo'),
+    version=get_property("__version__", "bingo"),
     author="Geoffrey Bomarito",
     author_email="geoffrey.f.bomarito@nasa.gov",
     description="A package for genetic optimization and symbolic regression",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nasa/bingo",
-    packages=["bingo",
-              "bingo.chromosomes",
-              "bingo.evaluation",
-              "bingo.evolutionary_algorithms",
-              "bingo.evolutionary_optimizers",
-              "bingo.local_optimizers",
-              "bingo.selection",
-              "bingo.stats",
-              "bingo.symbolic_regression",
-              "bingo.symbolic_regression.agraph",
-              "bingo.symbolic_regression.agraph.evaluation_backend",
-              "bingo.symbolic_regression.agraph.simplification_backend",
-              "bingo.symbolic_regression.benchmarking",
-              "bingo.util",
-              "bingo.variation",
-              ],
-    install_requires=["mpi4py",
-                      "numpy",
-                      "pytest",
-                      "pytest-mock",
-                      "pytest-timeout",
-                      "scipy",
-                      "dill",
-                      "sympy",
-                      "scikit-learn"],
-    python_requires='~=3.4',
+    packages=[
+        "bingo",
+        "bingo.chromosomes",
+        "bingo.evaluation",
+        "bingo.evolutionary_algorithms",
+        "bingo.evolutionary_optimizers",
+        "bingo.local_optimizers",
+        "bingo.selection",
+        "bingo.stats",
+        "bingo.symbolic_regression",
+        "bingo.symbolic_regression.agraph",
+        "bingo.symbolic_regression.agraph.evaluation_backend",
+        "bingo.symbolic_regression.agraph.simplification_backend",
+        "bingo.symbolic_regression.benchmarking",
+        "bingo.util",
+        "bingo.variation",
+    ],
+    install_requires=[
+        "mpi4py",
+        "numpy",
+        "pytest",
+        "pytest-mock",
+        "pytest-timeout",
+        "scipy",
+        "dill",
+        "sympy",
+        "scikit-learn",
+        "smcpy",
+    ],
+    python_requires="~=3.4",
     classifiers=[
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: Apache Software License",
-        "Development Status :: 3 - Alpha"
+        "Development Status :: 3 - Alpha",
     ],
     # add extension module
-    ext_modules=[CMakeExtension('bingocpp', 'bingocpp')],
+    ext_modules=[CMakeExtension("bingocpp", "bingocpp")],
     # add custom build_ext command
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,

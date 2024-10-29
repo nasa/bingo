c549d63ecab60544833341a868d004e70448d2d2
G. Bomarito <geoffrey.f.bomarito@nasa.gov>
2024-10-29 17:25:15 -0400

    correctly incrementing version and removing some stale code

--- setup.py
+++ setup.py
@@ -32,67 +32,6 @@ class CMakeExtension(Extension):
         self.sourcedir = os.path.abspath(sourcedir)
 
 
-# class CMakeBuild(build_ext):
-#     def run(self):
-#         try:
-#             out = subprocess.check_output(["cmake", "--version"])
-#         except OSError:
-#             warnings.warn(
-#                 "CMake must be installed to build the following extensions: "
-#                 + ", ".join(e.name for e in self.extensions)
-#             )
-#             return
-
-#         if platform.system() == "Windows":
-#             cmake_version = LooseVersion(
-#                 re.search(r"version\s*([\d.]+)", out.decode()).group(1)
-#             )
-#             if cmake_version < "3.1.0":
-#                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-#         try:
-#             for ext in self.extensions:
-#                 self.build_extension(ext)
-#         except subprocess.SubprocessError:
-#             warnings.warn("Couldn't install bingocpp")
-
-#     def build_extension(self, ext):
-#         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-#         cmake_args = [
-#             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
-#             "-DPYTHON_EXECUTABLE=" + sys.executable,
-#         ]
-
-#         cfg = "Debug" if self.debug else "Release"
-#         build_args = ["--config", cfg]
-
-#         if platform.system() == "Windows":
-#             cmake_args += [
-#                 "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
-#             ]
-#             if sys.maxsize > 2**32:
-#                 cmake_args += ["-A", "x64"]
-#             build_args += ["--", "/m"]
-#         else:
-#             cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
-#             build_args += ["--", "-j2"]
-
-#         env = os.environ.copy()
-#         env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-#             env.get("CXXFLAGS", ""), self.distribution.get_version()
-#         )
-#         if not os.path.exists(self.build_temp):
-#             os.makedirs(self.build_temp)
-#         subprocess.check_call(
-#             ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
-#         )
-#         subprocess.check_call(
-#             ["cmake", "--build", ".", "--target", "bingocpp"] + build_args,
-#             cwd=self.build_temp,
-#         )
-#         print()  # Add an empty line for cleaner output
-
-
 class CMakeBuild(build_ext):
     def build_extension(self, ext: CMakeExtension) -> None:
         # Must be in this form due to bug in .resolve() only fixed in Python 3.10+
